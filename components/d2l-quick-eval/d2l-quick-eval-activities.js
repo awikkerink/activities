b8484e68bfe2040171bd2a75629903419633538b
Carol Busman <Carol.Busman@D2L.com>
2019-05-28 08:53:48 -0400

    Adding in empty pages.

--- components/d2l-quick-eval/d2l-quick-eval-activities.js
+++ components/d2l-quick-eval/d2l-quick-eval-activities.js
@@ -5,6 +5,8 @@ import {mixinBehaviors} from '@polymer/polymer/lib/legacy/class.js';
 import './behaviors/d2l-quick-eval-siren-helper-behavior.js';
 import 'd2l-common/components/d2l-hm-filter/d2l-hm-filter.js';
 import 'd2l-common/components/d2l-hm-search/d2l-hm-search.js';
+import './d2l-quick-eval-no-submissions-image.js';
+import './d2l-quick-eval-no-criteria-results-image.js';
 
 /**
  * @customElement
@@ -26,6 +28,30 @@ class D2LQuickEvalActivities extends mixinBehaviors([D2L.PolymerBehaviors.QuickE
 					width: 250px;
 					margin-left: .25rem;
 				}
+				[hidden] {
+					display: none;
+				}
+				.d2l-quick-eval-no-submissions,
+				.d2l-quick-eval-no-criteria-results {
+					text-align: center;
+				}
+				d2l-quick-eval-no-submissions-image {
+					padding-top: 30px;
+					padding-bottom: 30px;
+					height: 35%;
+					width: 35%;
+				}
+				d2l-quick-eval-no-criteria-results-image {
+					padding-top: 30px;
+					padding-bottom: 30px;
+					height: 15%;
+					width: 15%;
+				}
+				.d2l-quick-eval-no-submissions-heading,
+				.d2l-quick-eval-no-criteria-results-heading {
+					@apply --d2l-heading-2;
+					margin: 0;
+				}
 			</style>
 			<div class="d2l-quick-eval-activity-list-modifiers">
 				<d2l-hm-filter>
@@ -36,11 +62,46 @@ class D2LQuickEvalActivities extends mixinBehaviors([D2L.PolymerBehaviors.QuickE
 				</d2l-hm-search>
 			</div>
 			<div class="clear"></div>
+			<div class="d2l-quick-eval-no-submissions" hidden$="[[!_shouldShowNoSubmissions(_data, filterApplied, searchCleared)]]">
+				<d2l-quick-eval-no-submissions-image></d2l-quick-eval-no-submissions-image>
+				<h2 class="d2l-quick-eval-no-submissions-heading">[[localize('caughtUp')]]</h2>
+				<p class="d2l-body-standard">[[localize('noSubmissions')]]</p>
+				<p class="d2l-body-standard">[[localize('checkBackOften')]]</p>
+			</div>
+			<div class="d2l-quick-eval-no-criteria-results" hidden$="[[!_shouldShowNoCriteriaResults(_data, filterApplied, searchCleared)]]">
+				<d2l-quick-eval-no-criteria-results-image></d2l-quick-eval-no-criteria-results-image>
+				<h2 class="d2l-quick-eval-no-criteria-results-heading">[[localize('noResults')]]</h2>
+				<p class="d2l-body-standard">[[localize('noCriteriaMatch')]]</p>
+			</div>
 		`;
 
 		quickEvalActivitiesTemplate.setAttribute('strip-whitespace', 'strip-whitespace');
 		return quickEvalActivitiesTemplate;
 	}
 	static get is() { return 'd2l-quick-eval-activities'; }
+	static get properties() {
+		return {
+			_data: {
+				type: Array,
+				value: []
+			},
+			filterApplied: {
+				type: Boolean,
+				value: false
+			},
+			searchCleared: {
+				type: Boolean,
+				value: true
+			}
+		};
+	}
+
+	_shouldShowNoSubmissions() {
+		return !(this._data.length) && !(this.filterApplied || !this.searchCleared);
+	}
+
+	_shouldShowNoCriteriaResults() {
+		return !(this._data.length) && (this.filterApplied || !this.searchCleared);
+	}
 }
 window.customElements.define(D2LQuickEvalActivities.is, D2LQuickEvalActivities);
