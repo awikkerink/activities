2ba558c55e61230a3d3f98670a3950f99ba091b1
Mohamed <mohamed.aboelsoud@d2l.com>
2019-07-24 16:35:55 -0400

    fixed minor bugs

--- components/d2l-quick-eval/activity-card/d2l-quick-eval-activity-card-unread-submissions.js
+++ components/d2l-quick-eval/activity-card/d2l-quick-eval-activity-card-unread-submissions.js
@@ -47,7 +47,7 @@ class D2LQuickEvalActivityCardUnreadSubmissions extends QuickEvalLocalize(Polyme
 			</style>
 			<div id="d2l-quick-eval-activity-card-submissions-container" class="d2l-quick-eval-activity-card-submissions-container">
 				<div class="d2l-quick-eval-activity-card-submissions-number">[[_getNewSubmissionsNumber(unread, resubmitted)]]</div>
-				<div class="d2l-quick-eval-activity-card-submissions-subtitle">[[_getNewSubmissionsSubtitle(unread, resubmitted, activityType)]]</div>
+				<div class="d2l-quick-eval-activity-card-submissions-subtitle">[[_getNewSubmissionsSubtitle(activityType)]]</div>
 			</div>
 			<d2l-tooltip for="d2l-quick-eval-activity-card-submissions-container" position="bottom">[[_getSubmissionTooltipText(unread, resubmitted, activityType)]]</d2l-tooltip>
 		`;
