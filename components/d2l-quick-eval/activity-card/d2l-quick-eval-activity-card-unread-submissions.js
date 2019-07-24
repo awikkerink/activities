eb9edcc144ecf73b2ef6016fd18668d579b36c46
Mohamed <mohamed.aboelsoud@d2l.com>
2019-07-24 14:47:02 -0400

    changed styling for new-submissions

--- components/d2l-quick-eval/activity-card/d2l-quick-eval-activity-card-unread-submissions.js
+++ components/d2l-quick-eval/activity-card/d2l-quick-eval-activity-card-unread-submissions.js
@@ -7,15 +7,37 @@ class D2LQuickEvalActivityCardUnreadSubmissions extends QuickEvalLocalize(Polyme
 	static get template() {
 		return html`
 			<style>
-				#d2l-quick-eval-activity-card-submissions {
-					text-align: center;
-					align-items: center;
-					display: flex;
-					justify-content: space-around;
+					.d2l-quick-eval-activity-card-submissions-container {
+						align-items: baseline;
+						display: flex;
+						justify-content: space-around;
+						margin: auto;
+					}
+					.d2l-quick-eval-activity-card-submissions-number {
+						font-size: 1.2rem;
+					}
+					.d2l-quick-eval-activity-card-submissions-subtitle {
+						font-size: 0.6rem;
+					}
+				@media (min-width: 525px) {
+					.d2l-quick-eval-activity-card-submissions-container {
+						align-items: center;
+						flex-direction: column;
+						margin: 0;
+					}
+					.d2l-quick-eval-activity-card-submissions-number {
+						font-size: 1.5rem;
+					}
+					.d2l-quick-eval-activity-card-submissions-subtitle {
+						font-size: 0.7rem;
+					}
 				}
 			</style>
-			<div id="d2l-quick-eval-activity-card-submissions">[[_getNewSubmissionsText(unread, resubmitted, activityType)]]</div>
-			<d2l-tooltip for="d2l-quick-eval-activity-card-submissions" position="bottom">[[_getSubmissionTooltipText(unread, resubmitted, activityType)]]</d2l-tooltip>
+			<div id="d2l-quick-eval-activity-card-submissions-container" class="d2l-quick-eval-activity-card-submissions-container">
+				<div class="d2l-quick-eval-activity-card-submissions-number">[[_getNewSubmissionsNumber(unread, resubmitted)]]</div>
+				<div class="d2l-quick-eval-activity-card-submissions-subtitle">[[_getNewSubmissionsSubtitle(unread, resubmitted, activityType)]]</div>
+			</div>
+			<d2l-tooltip for="d2l-quick-eval-activity-card-submissions-container" position="bottom">[[_getSubmissionTooltipText(unread, resubmitted, activityType)]]</d2l-tooltip>
 		`;
 	}
 	static get properties() {
@@ -52,8 +74,12 @@ class D2LQuickEvalActivityCardUnreadSubmissions extends QuickEvalLocalize(Polyme
 		};
 	}
 
-	_getNewSubmissionsText(unread, resubmitted, activityType) {
-		return this.localize(this.activityTypeLocalizeNew[activityType], 'num', this.unread + this.resubmitted);
+	_getNewSubmissionsNumber(unread, resubmitted) {
+		return this.unread + this.resubmitted;
+	}
+	_getNewSubmissionsSubtitle(unread, resubmitted, activityType) {
+		// return this.localize(this.activityTypeLocalizeNew[activityType], 'num', this.unread + this.resubmitted);
+		return this.localize(this.activityTypeLocalizeNew[activityType]);
 	}
 
 	_getSubmissionTooltipText(unread, resubmitted, activityType) {
