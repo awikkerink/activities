9833163359802d87611c45deb4626f975a567e7e
Dave Lockhart <dave.lockhart@d2l.com>
2019-07-26 17:44:26 -0400

    perceptual diff for summary container

---
+++ test/d2l-quick-eval/perceptual/d2l-quick-eval-search-results-summary-container.visual-diff.js
@@ -0,0 +1,35 @@
+const puppeteer = require('puppeteer');
+const VisualDiff = require('@brightspace-ui/visual-diff');
+
+describe('d2l-quick-eval-search-results-summary-container', function() {
+
+	const visualDiff = new VisualDiff('search-results-summary-container', __dirname);
+
+	let browser, page;
+
+	before(async() => {
+		browser = await puppeteer.launch();
+		page = await browser.newPage();
+		await page.setViewport({width: 800, height: 800, deviceScaleFactor: 2});
+		await page.goto(`${visualDiff.getBaseUrl()}/test/d2l-quick-eval/perceptual/d2l-quick-eval-search-results-summary-container.visual-diff.html`, {waitUntil: ['networkidle0', 'load']});
+		await page.bringToFront();
+	});
+
+	after(() => browser.close());
+
+	[
+		'zero-no-more',
+		'zero-more',
+		'one-no-more',
+		'one-more',
+		'many-no-more',
+		'many-more',
+		'rtl'
+	].forEach((name) => {
+		it(name, async function() {
+			const rect = await visualDiff.getRect(page, `#${name}`);
+			await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+		});
+	});
+
+});
