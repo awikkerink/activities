36fb2527af4bf0e90a27f97bec56f34d070a09b5
Carol Busman <Carol.Busman@D2L.com>
2019-08-19 09:24:38 -0400

    Adding quick eval subititle to activity card.

--- test/d2l-quick-eval/d2l-quick-eval-activity-card.js
+++ test/d2l-quick-eval/d2l-quick-eval-activity-card.js
@@ -24,28 +24,5 @@
 			assert.isTrue(qeActivityCard._denominatorOver99(100));
 			assert.isTrue(qeActivityCard._denominatorOver99(200));
 		});
-
-		test('_computeFormattedDueDate works as intended when activity has valid dueDate', function() {
-			const dueDate = '2012-09-01T13:00:00.000';
-			const expectedFormattedDate = '9/1/2012 1:00 PM';
-
-			assert.equal(qeActivityCard._computeFormattedDueDate(dueDate), expectedFormattedDate);
-		});
-
-		test('_computeFormattedDueDate works as intended when activity does not have dueDate', function() {
-			const dueDate = '';
-			const expectedFormattedDate = '';
-
-			assert.equal(qeActivityCard._computeFormattedDueDate(dueDate), expectedFormattedDate);
-		});
-
-		test('when dueDate changes, formattedDueDate also changes', function() {
-			let dueDate = '';
-			assert.equal(qeActivityCard._computeFormattedDueDate(dueDate), dueDate);
-
-			dueDate = '2016-03-07T00:20:00.000';
-			const expectedFormattedDate = '3/7/2016 12:20 AM';
-			assert.equal(qeActivityCard._computeFormattedDueDate(dueDate), expectedFormattedDate);
-		});
 	});
 })();
