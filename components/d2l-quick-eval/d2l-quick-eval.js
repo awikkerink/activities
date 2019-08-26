067eaac01251f81114939faddf5648ba7e9a16ec
Alex Bedley <alex.bedley@D2L.com>
2019-08-26 11:32:34 -0400

    US109607 - fix bug if you were previously on activities view and then we disabled feature flag

--- components/d2l-quick-eval/d2l-quick-eval.js
+++ components/d2l-quick-eval/d2l-quick-eval.js
@@ -104,7 +104,7 @@ class D2LQuickEval extends  QuickEvalLocalize(PolymerElement) {
 	static get is() { return 'd2l-quick-eval'; }
 
 	_toggleView(e) {
-		if (e.detail.view === 'submissions') {
+		if (e.detail.view === 'submissions' || !this.activitiesViewEnabled) {
 			this._showActivitiesView = false;
 		} else {
 			this._showActivitiesView = true;
