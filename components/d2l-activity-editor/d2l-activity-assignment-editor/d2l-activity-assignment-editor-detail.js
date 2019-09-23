33738e79f8d4dcbb23a7c1cde011d2a952e47521
Margaret Dulat <Margaret.Dulat@D2L.com>
2019-09-23 16:48:58 -0700

    US110193 display assignment name, non-ediable if no update-name action present

--- components/d2l-activity-editor/d2l-activity-assignment-editor/d2l-activity-assignment-editor-detail.js
+++ components/d2l-activity-editor/d2l-activity-assignment-editor/d2l-activity-assignment-editor-detail.js
@@ -2,6 +2,7 @@ import { css, html, LitElement } from 'lit-element/lit-element.js';
 import { AssignmentEntity } from 'siren-sdk/src/activities/assignments/AssignmentEntity.js';
 import { EntityMixinLit } from 'siren-sdk/src/mixin/entity-mixin-lit.js';
 import { LocalizeMixin } from '@brightspace-ui/core/mixins/localize-mixin.js';
+import 'd2l-inputs/d2l-input-text.js';
 
 class AssignmentEditorDetail extends EntityMixinLit(LocalizeMixin(LitElement)) {
 
@@ -15,6 +16,7 @@ class AssignmentEditorDetail extends EntityMixinLit(LocalizeMixin(LitElement)) {
 		return css`
 			:host {
 				display: inline-block;
+				padding: 20px;
 			}
 			:host([hidden]) {
 				display: none;
@@ -58,13 +60,36 @@ class AssignmentEditorDetail extends EntityMixinLit(LocalizeMixin(LitElement)) {
 	_onAssignmentChange(assignment) {
 		if (assignment) {
 			this._name = assignment.name();
+
+			if (assignment.canEditName()) {
+				this.shadowRoot.getElementById('assignment-name').removeAttribute('disabled', '');
+			} else {
+				this.shadowRoot.getElementById('assignment-name').setAttribute('disabled', '');
+			}
 		}
+	}
+
+	_saveName() {
+
+	}
+
+	_saveNameOnInput() {
+
 	}
 
 	render() {
 		return html`
-			<h2>Hello ${this._name}!</h2>
-			<div>Localization Example: ${this.localize('myLangTerm')}</div>
+			<div id="assignment-name-container">
+				<label for="assignment-name">${this.localize('name')}*</label>
+				<d2l-input-text
+					id="assignment-name"
+					value="${this._name}"
+					on-change="${this._saveName()}"
+					on-input="${this._saveNameOnInput()}"
+					aria-label="${this.localize('name')}"
+					prevent-submit>
+				</d2l-input-text>
+			</div>
 		`;
 	}
 }
--- components/d2l-activity-editor/d2l-activity-assignment-editor/lang/en.js
+++ components/d2l-activity-editor/d2l-activity-assignment-editor/lang/en.js
@@ -1,3 +1,3 @@
 export const assignment = {
-	'myLangTerm': 'I am a dynamically imported localized string!'
+	'name': 'Name'
 };
