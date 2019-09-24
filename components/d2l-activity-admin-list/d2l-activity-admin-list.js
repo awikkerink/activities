74443fd526c14807c8f38e78361d65dda354476b
Justin Lai <Justin.Lai@D2L.com>
2019-09-24 15:59:56 -0400

    Adding skeleton for activity-admin-list component to support Learning Paths

---
+++ components/d2l-activity-admin-list/d2l-activity-admin-list.js
@@ -0,0 +1,25 @@
+import { css, html, LitElement } from 'lit-element/lit-element.js';
+
+class AdminList extends LitElement {
+	static get properties() {
+		return {};
+	}
+
+	static get styles() {
+		return css`
+			:host {
+				display: block;
+			}
+			:host([hidden]) {
+				display: none;
+			}
+		`;
+	}
+
+	render() {
+		return html`
+			<div></div>
+		`;
+	}
+}
+customElements.define('d2l-activity-admin-list', AdminList);
