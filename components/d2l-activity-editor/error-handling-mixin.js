1716da99e5ade3a4c4bef91bf253e3aa8da94c53
Vincent Wong <Vincent.Wong@D2L.com>
2019-10-01 11:13:09 -0700

    Create error handling mixin for setting and clearing errors

---
+++ components/d2l-activity-editor/error-handling-mixin.js
@@ -0,0 +1,25 @@
+import { LocalizeMixin } from '@brightspace-ui/core/mixins/localize-mixin.js';
+
+export const ErrorHandlingMixin = superclass => class extends superclass {
+	constructor() {
+		super();
+	}
+
+	async setError(errorProperty, langterm, tooltipId) {
+		this[errorProperty] = this.localize(langterm);
+
+		await this.updateComplete;
+		this._showTooltip(tooltipId);
+	}
+
+	clearError(errorProperty) {
+		this[errorProperty] = '';
+	}
+
+	_showTooltip(tooltipId) {
+		const tooltipElem = this.shadowRoot.getElementById(tooltipId);
+		if (tooltipElem) {
+			tooltipElem.show();
+		}
+	}
+};
