1ca0ef496766057e5879393d2a47a576d857626c
Justin Lai <Justin.Lai@D2L.com>
2019-10-02 10:34:12 -0400

    basic activity admin list

--- components/d2l-activity-admin-list/d2l-activity-admin-list.js
+++ components/d2l-activity-admin-list/d2l-activity-admin-list.js
@@ -1,8 +1,18 @@
 import { css, html, LitElement } from 'lit-element/lit-element.js';
+import 'd2l-common/components/d2l-hm-search/d2l-hm-search.js';
+import 'd2l-typography/d2l-typography-shared-styles.js';
+import 'd2l-button/d2l-button.js';
 
 class AdminList extends LitElement {
 	static get properties() {
-		return {};
+		return {
+			header: {
+				type: String
+			},
+			collectionHref: {
+				type: String
+			},
+		};
 	}
 
 	static get styles() {
@@ -13,12 +23,23 @@ class AdminList extends LitElement {
 			:host([hidden]) {
 				display: none;
 			}
+			.d2l-activity-admin-list-container {
+				display: flex;
+				justify-content: space-between;
+				align-items: baseline;
+			}
 		`;
 	}
 
 	render() {
 		return html`
-			<div>D2L Activity Admin List</div>
+		<div class="d2l-activity-admin-list-container">
+			<h1>${this.title}</h1>
+			<d2l-button primary>Create Learning Path</d2l-button>
+		</div>
+		<div>
+			CollectionHref: ${this.collectionHref}
+		</div>
 		`;
 	}
 }
