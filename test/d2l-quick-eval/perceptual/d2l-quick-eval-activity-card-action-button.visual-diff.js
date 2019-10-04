dffde16868638e8edda42f6b9dbd505add4a146f
Carol Busman <Carol.Busman@D2L.com>
2019-10-04 15:05:50 -0400

    Adding visual diff tests for action buttons.

---
+++ test/d2l-quick-eval/perceptual/d2l-quick-eval-activity-card-action-button.visual-diff.js
@@ -0,0 +1,52 @@
+const puppeteer = require('puppeteer');
+const VisualDiff = require('@brightspace-ui/visual-diff');
+
+describe('d2l-quick-eval-activity-card-action-button', function() {
+
+	const visualDiff = new VisualDiff('activity-card-action-button', __dirname);
+
+	let browser, page;
+
+	before(async() => {
+		browser = await puppeteer.launch();
+		page = await browser.newPage();
+		await page.setViewport({width: 900, height: 800, deviceScaleFactor: 2});
+		await page.goto(`${visualDiff.getBaseUrl()}/test/d2l-quick-eval/perceptual/d2l-quick-eval-activity-card-action-button.visual-diff.html`, {waitUntil: ['networkidle0', 'load']});
+		await page.bringToFront();
+	});
+
+	after(() => browser.close());
+
+	[
+		'text-and-icon',
+		'button-disabled',
+		'icon-only',
+		'text-only',
+		'tall-height',
+		'small-screen-width'
+	].forEach((name) => {
+		describe(name, () => {
+			[ 'normal', 'hover', 'focus' ].forEach((state) => {
+				it(state, async function() {
+					switch(state) {
+						case 'hover':
+							await page.hover(`.${name}`);
+							break;
+						case 'focus':
+							await page.focus(`.${name}`);
+							break;
+					}
+					if (name.indexOf('width') !== -1) {
+						if (name.indexOf('small') !== -1 ) {
+							await page.setViewport({width: 899, height: 800, deviceScaleFactor: 2});
+						}
+					}
+					const rect = await visualDiff.getRect(page, `#${name}`);
+					await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+				});
+			});
+		});
+		
+	});
+
+});
