62089b7d23d29cfb6f307631d7e2a73ae4cf4a57
Ryan Mahler <ryan.mahler@d2l.com>
2019-10-31 14:52:21 -0700

    US111620 Add plaintext instructions editor
    This passes down a user config setting to determine whether to use the HTML editor or a plaintext editor for the instructions. This could be represented in an API response somehow, but after some thought and trying to flesh that out, this was a much simpler solution. It's unfortunate that the interim components needs to pass down an attribute that they don't really care about (`htmlEditorEnabled`), but it's not all that much more code or complexity, and the presence of other text editing fields in the future would be easy to support.
    
    The new `d2l-activity-text-editor` acts more-or-less the same as the HTML editor did, but now will display a `d2l-input-textarea` when the user doesn't want to use richtext editing.

---
+++ components/d2l-activity-editor/d2l-activity-text-editor.js
@@ -0,0 +1,64 @@
+import 'd2l-inputs/d2l-input-textarea';
+import './d2l-activity-html-editor';
+import { html, LitElement } from 'lit-element/lit-element';
+
+class ActivityTextEditor extends LitElement {
+
+	static get properties() {
+		return {
+			value: { type: String },
+			htmlEditorEnabled: { type: Boolean },
+			richtextEditorConfig: { type: Object },
+			disabled: { type: Boolean },
+			ariaLabel: { type: String },
+		};
+	}
+
+	_onPlaintextChange() {
+		const content = this.shadowRoot.querySelector('d2l-input-textarea').value;
+		this._dispatchChangeEvent(content);
+	}
+
+	_onRichtextChange(e) {
+		const content = e.detail.content;
+		this._dispatchChangeEvent(content);
+	}
+
+	_dispatchChangeEvent(content) {
+		this.dispatchEvent(new CustomEvent('d2l-activity-text-editor-change', {
+			bubbles: true,
+			composed: true,
+			detail: {
+				content: content
+			}
+		}));
+	}
+
+	render() {
+		if (this.htmlEditorEnabled) {
+			return html`
+				<d2l-activity-html-editor
+					ariaLabel="${this.ariaLabel}"
+					value="${this.value}"
+					?htmlEditorEnabled="${this.htmlEditorEnabled}"
+					?disabled="${this.disabled}"
+					@d2l-activity-html-editor-change="${this._onRichtextChange}"
+					.richtextEditorConfig="${this.richtextEditorConfig}">
+				</d2l-activity-html-editor>
+			`;
+		} else {
+			return html`
+				<d2l-input-textarea
+					id="text-editor"
+					value="${this.value}"
+					?disabled="${this.disabled}"
+					@change="${this._onPlaintextChange}"
+					@input="${this._onPlaintextChange}">
+				</d2l-input-textarea>
+			`;
+		}
+	}
+
+}
+
+customElements.define('d2l-activity-text-editor', ActivityTextEditor);
