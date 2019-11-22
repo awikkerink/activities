806185a1218b7e796a43720d1eca2b6299cb6bc3
Mohamed <mohamed.aboelsoud@d2l.com>
2019-11-22 11:48:48 -0500

    more lint fixes

--- components/d2l-quick-eval/lang/insert-lang-term.js
+++ components/d2l-quick-eval/lang/insert-lang-term.js
@@ -2,17 +2,17 @@
 
 'use strict';
 
-var fs = require('fs');
-var path = require('path');
+const fs = require('fs');
+const path = require('path');
 
-const LANG_PATH = path.resolve("components/d2l-quick-eval/lang");
+const LANG_PATH = path.resolve('components/d2l-quick-eval/lang');
 
 const traverseJSONDir = (pathName, cb) => {
 	fs.readdirSync(pathName)
-		.filter((x) => path.extname(x) == ".json")
+		.filter((x) => path.extname(x) === '.json')
 		.map(x => path.resolve(pathName, x))
 		.forEach(cb);
-}
+};
 
 function sortObject(obj) {
 	return Object.keys(obj).sort().reduce(function(result, key) {
@@ -25,34 +25,32 @@ function addJsonLangTerm(langKey, langPhrase, filePath) {
 	const parsedJSON = JSON.parse(fs.readFileSync(filePath));
 	parsedJSON[langKey] = langPhrase;
 	const sortedObject = sortObject(Object.assign({}, parsedJSON));
-	fs.writeFileSync(filePath, JSON.stringify(sortedObject, null, 3).replace(/": "/g, '" : "') + "\n");
+	fs.writeFileSync(filePath, JSON.stringify(sortedObject, null, 3).replace(/": "/g, '" : "') + '\n');
 }
 
 function removeJsonLangTerm(langKey, filePath) {
 	const parsedJSON = JSON.parse(fs.readFileSync(filePath));
-	delete parsedJSON[langKey]
-	fs.writeFileSync(filePath, JSON.stringify(parsedJSON, null, 3).replace(/": "/g, '" : "') + "\n");
+	delete parsedJSON[langKey];
+	fs.writeFileSync(filePath, JSON.stringify(parsedJSON, null, 3).replace(/": "/g, '" : "') + '\n');
 }
 
 function CLIAppendLangTerm(langKey, langPhrase) {
-	if (!langPhrase) throw "add the lang term that you want to add/modify and the content of the lang term"
+	if (!langPhrase) throw 'add the lang term that you want to add/modify and the content of the lang term';
 	traverseJSONDir(LANG_PATH, addJsonLangTerm.bind(null, langKey, langPhrase));
-	// CLIExecuteBuild();
 }
 
 function CLIRemoveLangTerm(langKey) {
-	if (!langKey) throw "add the lang term that you want to remove"
+	if (!langKey) throw 'add the lang term that you want to remove';
 	traverseJSONDir(LANG_PATH, removeJsonLangTerm.bind(null, langKey));
 }
 
 function CLISyncJsonFiles() {
-	const enJsonFile = path.resolve(LANG_PATH, "en.json");
-	const jaJsonFile = path.resolve(LANG_PATH, "ja.json");
+	const enJsonFile = path.resolve(LANG_PATH, 'en.json');
+	const jaJsonFile = path.resolve(LANG_PATH, 'ja.json');
 	const parsedEnJson = JSON.parse(fs.readFileSync(enJsonFile));
 	const parsedJaJson = JSON.parse(fs.readFileSync(jaJsonFile));
 
 	// check for new terms
-	console.log("checking en.json for new or removed lang terms");
 	Object.keys(parsedEnJson).forEach((key) => {
 		if (!parsedJaJson.hasOwnProperty(key)) {
 			CLIAppendLangTerm(key, parsedEnJson[key]);
@@ -60,7 +58,6 @@ function CLISyncJsonFiles() {
 	});
 
 	// check for removed terms
-	console.log("checking en.json for new or removed lang terms");
 	Object.keys(parsedJaJson).forEach((key) => {
 		if (!parsedEnJson.hasOwnProperty(key)) {
 			CLIRemoveLangTerm(key);
@@ -71,19 +68,16 @@ function CLISyncJsonFiles() {
 const params = process.argv.slice(2);
 const [command, langKey, langPhrase] = params;
 switch (command) {
-	case "sync":
-		console.log("syncing...");
+	case 'sync':
 		CLISyncJsonFiles();
 		break;
-	case "add":
-	case "change": // add and change do the same thing, but easier in terms of usability
-		console.log("add or changing...");
+	case 'add':
+	case 'change': // add and change do the same thing, but easier in terms of usability
 		CLIAppendLangTerm(langKey, langPhrase);
 		break;
-	case "remove":
-		console.log("removing...");
+	case 'remove':
 		CLIRemoveLangTerm(langKey, langPhrase);
 		break;
 	default:
-		throw "invalid command";
+		throw 'invalid command';
 }
