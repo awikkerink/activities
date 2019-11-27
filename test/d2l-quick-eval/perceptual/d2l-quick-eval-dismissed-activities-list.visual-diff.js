aba84ccd5e5fd8a685b9ca2d848159663f78b56f
Carol Busman <Carol.Busman@D2L.com>
2019-11-27 14:44:20 -0500

    Adding dismissed activities list.

---
+++ test/d2l-quick-eval/perceptual/d2l-quick-eval-dismissed-activities-list.visual-diff.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer');
+const VisualDiff = require('@brightspace-ui/visual-diff');
+
+describe('d2l-quick-eval-dismissed-activities-list', function() {
+
+	const visualDiff = new VisualDiff('dismissed-activities-list', __dirname);
+
+	let browser, page;
+
+	before(async() => {
+		browser = await puppeteer.launch();
+		page = await browser.newPage();
+		await page.setViewport({width: 900, height: 800, deviceScaleFactor: 2});
+		await page.goto(`${visualDiff.getBaseUrl()}/test/d2l-quick-eval/perceptual/d2l-quick-eval-dismissed-activities-list.visual-diff.html`, {waitUntil: ['networkidle0', 'load']});
+		await page.bringToFront();
+	});
+
+	after(() => browser.close());
+	it ('default', async function() {
+		const rect = await visualDiff.getRect(page, '#default');
+		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+	});
+
+});
