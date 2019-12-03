3cb7078277ed59074d5599d0642bf9de53656912
Ryan Mahler <Ryan.Mahler@D2L.com>
2019-12-03 09:29:39 -0800

    US112016 Add attachment list
    This adds a list view of (link) attachments in the editor, as well as support for removing them.
    
    Thanks to the entity store and a small change in the LMS that adds `Link` headers to the add/delete attachment routes, this approach automatically re-renders the list when an attachment is added or removed without any additional effort. It's likely that once autosave is figured out a bit more, we'll have to add better interim states, but that remains to be seen.

---
+++ components/d2l-activity-editor/d2l-activity-attachments/d2l-activity-attachments-list.js
@@ -0,0 +1,62 @@
+import './d2l-activity-attachment';
+import { css, html, LitElement } from 'lit-element/lit-element';
+import { AttachmentCollectionEntity } from 'siren-sdk/src/activities/AttachmentCollectionEntity';
+import { EntityMixinLit } from 'siren-sdk/src/mixin/entity-mixin-lit';
+import { repeat } from 'lit-html/directives/repeat';
+
+class ActivityAttachmentsList extends EntityMixinLit(LitElement) {
+	static get properties() {
+		return {
+			_attachmentUrls: { type: Array }
+		};
+	}
+
+	static get styles() {
+		return css`
+			:host {
+				display: block;
+			}
+			:host > * {
+				margin-bottom: 20px;
+			}
+		`;
+	}
+
+	constructor() {
+		super();
+		this._setEntityType(AttachmentCollectionEntity);
+	}
+
+	set _entity(entity) {
+		if (!this._entityHasChanged(entity)) {
+			return;
+		}
+
+		if (entity) {
+			const attachments = entity.getAttachmentEntities();
+			this._attachmentUrls = attachments.map(attachment => {
+				if (attachment.href) {
+					return attachment.href;
+				}
+
+				// TODO use self link not this nonsense
+				return attachment.getActionByName('delete').href;
+			})
+		}
+
+		super._entity = entity;
+	}
+
+	render() {
+		return html`
+			${repeat(this._attachmentUrls, href => href, href => html`
+				<d2l-activity-attachment
+					.href="${href}"
+					.token="${this.token}">
+				</d2l-activity-attachment>
+			`)}
+		`;
+	}
+}
+
+customElements.define('d2l-activity-attachments-list', ActivityAttachmentsList);
