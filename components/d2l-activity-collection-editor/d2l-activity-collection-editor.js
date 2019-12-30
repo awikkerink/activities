bbb6b67011c7647a61ad9e33421257d01811bd27
Maya Jones <Maya.Jones@D2L.com>
2019-12-30 17:07:52 -0500

    Loading skeleton not responsive

--- components/d2l-activity-collection-editor/d2l-activity-collection-editor.js
+++ components/d2l-activity-collection-editor/d2l-activity-collection-editor.js
@@ -1,5 +1,7 @@
 import { css, html, LitElement } from 'lit-element/lit-element.js';
 import { repeat } from 'lit-html/directives/repeat';
+import { until } from 'lit-html/directives/until.js';
+import { cache } from 'lit-html/directives/cache.js';
 import { heading1Styles, heading4Styles, bodyCompactStyles, labelStyles} from '@brightspace-ui/core/components/typography/styles.js';
 import { EntityMixinLit } from 'siren-sdk/src/mixin/entity-mixin-lit.js';
 import { ActivityUsageEntity } from 'siren-sdk/src/activities/ActivityUsageEntity.js';
@@ -32,6 +34,8 @@ class CollectionEditor extends LocalizeMixin(EntityMixinLit(LitElement)) {
 		this._items = [];
 		this._candidateItems = [];
 		this._specialization = {};
+		this._showImages = false;
+		this._loadedImages = 0;
 		this._setEntityType(ActivityUsageEntity);
 	}
 
@@ -58,7 +62,9 @@ class CollectionEditor extends LocalizeMixin(EntityMixinLit(LitElement)) {
 		this._canEditDraft = usage.canEditDraft();
 		this._setVisibility = (draftStatus) => {
 			this._isDraft = draftStatus;
-			usage.setDraftStatus(draftStatus).then(() => usage.update());
+			usage.setDraftStatus(draftStatus).then(() => {
+				usage.update()
+			});
 		};
 
 		let hasACollection = false;
@@ -88,10 +94,11 @@ class CollectionEditor extends LocalizeMixin(EntityMixinLit(LitElement)) {
 			this._addExistingAction = collection._entity.getActionByName('start-add-existing-activity');
 		});
 
-		usage.subEntitiesLoaded().then(() => {
+		this._mainPageLoad = usage.subEntitiesLoaded().then(() => {
 			if (!hasACollection) {
 				this._items = [];
 			}
+			this._loaded = true;
 		});
 	}
 
@@ -162,6 +169,7 @@ class CollectionEditor extends LocalizeMixin(EntityMixinLit(LitElement)) {
 			_items: { type: Array },
 			_name: { type: String },
 			_selectionCount: { type: Number },
+			_showImages: {type: Boolean },
 			_specialization: { type: Object }
 		};
 	}
@@ -208,6 +216,7 @@ class CollectionEditor extends LocalizeMixin(EntityMixinLit(LitElement)) {
 				max-width: 1230px;
 				overflow: hidden;
 				padding: 0 0.3rem;
+				position: relative;
 			}
 			.d2l-activity-collection-header-col1 {
 				max-width: 600px;
@@ -251,6 +260,56 @@ class CollectionEditor extends LocalizeMixin(EntityMixinLit(LitElement)) {
 			}
 			.d2l-activity-collection-toggle-container-button {
 				display: none;
+			}
+
+			.d2l-activity-collection-header-1-skeleton {
+				height: 2.4rem;
+				display: flex;
+				align-items: center;
+				min-width: 20rem;
+			}
+
+			.d2l-activity-collection-header-1-skeleton-svg {
+				max-height: 1.45rem;
+			}
+
+			.d2l-activity-collection-body-compact-skeleton {
+				height: 3.6rem;
+				display: flex;
+				align-items: center;
+				min-width: 25rem;
+			}
+
+			.d2l-activity-collection-body-compact-skeleton-svg {
+				height: 0.55rem;
+			}
+
+			.d2l-activity-collection-body-small-skeleton-svg {
+				height: 0.5rem;
+			}
+
+			.d2l-activity-collection-list-actions-skeleton {
+				align-self: center;
+			}
+
+			.d2l-activitiy-collection-list-item-illustration {
+				display: flex;
+				position: relative;
+			}
+
+			.d2l-activitiy-collection-organization-image {
+				top: 0;
+				position: absolute;
+			}
+
+			@keyframes loadingPulse {
+				0% { fill: var(--d2l-color-sylvite); }
+				50% { fill: var(--d2l-color-regolith); }
+				100% { fill: var(--d2l-color-sylvite); }
+			}
+
+			.d2l-activity-collection-skeleton-rect {
+				animation: loadingPulse 1.8s linear infinite;
 			}
 
 			@media only screen and (max-width: 929px) {
@@ -301,58 +360,165 @@ class CollectionEditor extends LocalizeMixin(EntityMixinLit(LitElement)) {
 	}
 
 	render() {
+		if (!this._mainPageLoad) {
+			return null;
+		}
+
 		const icon = (this._isDraft ? 'tier1:visibility-hide' : 'tier1:visibility-show');
 
-		const items = repeat(this._items, (item) => item.self(), item =>
-			html`
-			<d2l-list-item>
-				<d2l-organization-image href=${item.self()} slot="illustration"></d2l-organization-image>
-				<d2l-list-item-content>
-					${item.name()}
-					<div slot="secondary">${item.hasClass(organizationClasses.courseOffering) ? this.localize('course') : null}</div>
-				</d2l-list-item-content>
-				<d2l-button-icon slot="actions" text="${this.localize('removeActivity', 'courseName', item.name())}" icon="d2l-tier1:close-default" @click=${item.removeItem}>
-			</d2l-list-item>
+		const learningPathTitle = this._mainPageLoad.then(() => {
+			return html`
+				<h1 class="d2l-heading-1 d2l-activity-collection-title-header">
+					<d2l-labs-edit-in-place size="49" placeholder="${this.localize('untitledLearningPath')}" maxlength="128" value="${this._name}" @change=${this._titleChanged}></d2l-labs-edit-in-place>
+				</h1>
 			`
-		);
+		});
+		const learningPathTitleSketeton = html`
+			<div class="d2l-activity-collection-title-header d2l-activity-collection-header-1-skeleton">
+				<svg width="100%" class="d2l-activity-collection-header-1-skeleton-svg">
+					<rect x="0" width="70%" y="0" height="100%" stroke="none" rx="4" class="d2l-activity-collection-skeleton-rect"></rect>
+				</svg>
+			</div>
+		`;
+
+		const learningPathDescription = this._mainPageLoad.then(() => {
+			return html`
+				<div class="d2l-body-compact d2l-activity-collection-description">
+					<d2l-labs-edit-in-place size="49" placeholder="${this.localize('enterADescription')}" maxlength="280" value="${this._description}" @change=${this._descriptionChanged}></d2l-labs-edit-in-place>
+				</div>
+			`
+		});
+		const learningPathDescriptionSketeton = html`
+			<div class="d2l-activity-collection-description d2l-activity-collection-body-compact-skeleton">
+				<svg width="100%" height="100%">
+					<rect x="0" width="100%" y="6" stroke="none" rx="4" class="d2l-activity-collection-skeleton-rect d2l-activity-collection-body-compact-skeleton-svg"></rect>
+					<rect x="0" width="100%" y="30" stroke="none" rx="4" class="d2l-activity-collection-skeleton-rect d2l-activity-collection-body-compact-skeleton-svg"></rect>
+					<rect x="0" width="80%" y="54" stroke="none" rx="4" class="d2l-activity-collection-skeleton-rect d2l-activity-collection-body-compact-skeleton-svg"></rect>
+				</svg>
+			</div>
+		`;
+
+		const learningPathVisibilityToggle = this._handleFirstLoad(() => {
+			return html`
+				<d2l-activity-visibility-editor class="d2l-activity-collection-toggle-container" ?disabled="${!this._items.length}" .href="${this.href}" .token="${this.token}"></d2l-activity-visibility-editor>
+				<d2l-button-icon
+					class="d2l-activity-collection-toggle-container-button"
+					?disabled="${!this._canEditDraft || this.disabled}"
+					@click="${() => typeof this._setVisibility === 'function' && this._setVisibility(!this._isDraft)}"
+					icon=${icon}>
+				</d2l-button-icon>
+			`
+		}, () => {
+			return html`
+				<div class="d2l-activity-collection-toggle-container">
+					<svg viewBox="0 0 150 38" width="150" height="38">
+						<rect x="1" width="60" y="5" height="30" stroke="none" rx="4" class="d2l-activity-collection-skeleton-rect"></rect>
+						<rect x="72" width="90" y="15" stroke="none" rx="4" class="d2l-activity-collection-skeleton-rect d2l-activity-collection-body-compact-skeleton-svg"></rect>
+					</svg>
+				</div>
+			`
+		});
+
+		const addActivityButton = this._handleFirstLoad(() => {
+			return html`<d2l-button @click="${this.open}" primary>${this.localize('addActivity')}</d2l-button>`;
+		},	() => {
+			return html`
+				<svg viewBox="0 0 142 42" width="142" height="42" class="d2l-activity-collection-list-actions-skeleton">
+					<rect x="0" width="142" y="0" height="42" stroke="none" rx="4" class="d2l-activity-collection-skeleton-rect"></rect>
+				</svg>
+			`;
+		});
+
+		const activityCount = this._handleFirstLoad(() => {
+			return this.localize('numberOfActivities', 'count', this._items.length);
+		},	() => {
+			return html`
+				<svg width="90" class="d2l-activity-collection-body-compact-skeleton-svg d2l-activity-collection-list-actions-skeleton">
+					<rect x="0" width="100%" y="0" height="100%" stroke="none" rx="4" class="d2l-activity-collection-skeleton-rect"></rect>
+				</svg>
+			`;
+		});
+
+		const items = this._handleFirstLoad(this._renderItemList.bind(this), () => html`${this._renderItemListSkeleton(5)}`);
 
 		return html`
 			<div class="d2l-activity-collection-header">
 				<div class="d2l-activity-collection-header-content">
 					<div class="d2l-heading-4 d2l-activity-collection-sub-header">${this.localize('editLearningPath')}</div>
 					<div class="d2l-activity-collection-base-info">
-						<div class="d2l-activity-collection-header-col1">
-							<h1 class="d2l-heading-1 d2l-activity-collection-title-header">
-								<d2l-labs-edit-in-place size="49" placeholder="${this.localize('untitledLearningPath')}" maxlength="128" value="${this._name}" @change=${this._titleChanged}></d2l-labs-edit-in-place>
-							</h1>
-							<div class="d2l-body-compact d2l-activity-collection-description">
-								<d2l-labs-edit-in-place size="49" placeholder="${this.localize('enterADescription')}" maxlength="280" value="${this._description}" @change=${this._descriptionChanged}></d2l-labs-edit-in-place>
-							</div>
+						<div class="d2l-activity-collection-header-col1" style="position: relative">
+							${until(learningPathTitle, learningPathTitleSketeton)}
+							${until(learningPathDescription, learningPathDescriptionSketeton)}
 						</div>
-						<d2l-activity-visibility-editor class="d2l-activity-collection-toggle-container" ?disabled="${!this._items.length}" .href="${this.href}" .token="${this.token}"></d2l-activity-visibility-editor>
-						<d2l-button-icon
-							class="d2l-activity-collection-toggle-container-button"
-							?disabled="${!this._canEditDraft || this.disabled}"
-							@click="${() => typeof this._setVisibility === 'function' && this._setVisibility(!this._isDraft)}"
-							icon=${icon}>
-						</d2l-button-icon>
+						${learningPathVisibilityToggle}
 					</div>
 				</div>
 			</div>
 			<div class="d2l-activity-collection-body">
 				<div class="d2l-activity-collection-body-content">
 					<div class="d2l-activity-collection-list-actions">
-						<d2l-button @click="${this.open}" primary>${this.localize('addActivity')}</d2l-button>
-						<div class="d2l-body-compact">${this.localize('numberOfActivities', 'count', this._items.length)}</div>
+						${addActivityButton}
+						<div class="d2l-body-compact">${activityCount}</div>
 					</div>
 					<div class="d2l-activity-collection-activities">
-						<d2l-list>${items}</d2l-list>
+						${items}
 					</div>
 				</div>
 			</div>
 			${this._renderCandidates()}
-
 		`;
+	}
+
+	_handleFirstLoad(whenLoaded, whileLoading = () => null) {
+		return this._loaded ? whenLoaded() : until(this._mainPageLoad.then(whenLoaded), whileLoading());
+	}
+
+	_renderItemList() {
+		const items = repeat(this._items, (item) => item.self(), item => {
+			return html`
+				<d2l-list-item>
+					<div slot="illustration" class="d2l-activitiy-collection-list-item-illustration">
+						${this._renderCourseImageSkeleton()}
+						<d2l-organization-image class="d2l-activitiy-collection-organization-image" href=${item.self()} @d2l-organization-image-loaded="${this._onListImageLoaded}" ?hidden="${!this._showImages}"></d2l-organization-image>
+					</div>
+					<d2l-list-item-content>
+						${item.name()}
+						<div slot="secondary">${item.hasClass(organizationClasses.courseOffering) ? this.localize('course') : null}</div>
+					</d2l-list-item-content>
+					<d2l-button-icon slot="actions" text="${this.localize('removeActivity', 'courseName', item.name())}" icon="d2l-tier1:close-default" @click=${item.removeItem}>
+				</d2l-list-item>
+			`
+		});
+
+		return html`<d2l-list>${items}</d2l-list>`;
+	}
+
+	_renderCourseImageSkeleton() {
+		return html`
+			<svg viewBox="0 0 180 77" width="100%" slot="illustration">
+				<rect x="0" width="100%" y="0" height="100%" stroke="none" class="d2l-activity-collection-skeleton-rect"></rect>
+			</svg>
+		`;
+	}
+
+	_renderItemListSkeleton(numberOfItems) {
+		const itemsSkeleton = html`
+			<d2l-list-item>
+				${this._renderCourseImageSkeleton()}
+				<d2l-list-item-content>
+					<svg width="100%" class="d2l-activity-collection-body-compact-skeleton-svg">
+						<rect x="0" width="25%" y="0" height="100%" stroke="none" rx="4" class="d2l-activity-collection-skeleton-rect"></rect>
+					</svg>
+					<div slot="secondary">
+						<svg width="100%" class="d2l-activity-collection-body-small-skeleton-svg">
+							<rect x="0" width="10%" y="0" height="100%" stroke="none" rx="4" class="d2l-activity-collection-skeleton-rect"></rect>
+						</svg>
+					</div>
+				</d2l-list-item-content>
+				<d2l-button-icon slot="actions" text="${this.localize('removeActivity', 'courseName', 'Title')}" icon="d2l-tier1:close-default" disabled>
+			</d2l-list-item>
+		`;
+		return html`<d2l-list>${(new Array(numberOfItems)).fill(itemsSkeleton)}</d2l-list>`;
 	}
 
 	_renderCandidates() {
@@ -393,6 +559,13 @@ class CollectionEditor extends LocalizeMixin(EntityMixinLit(LitElement)) {
 				</d2l-dialog>
 
 		`;
+	}
+
+	_onListImageLoaded() {
+		this._loadedImages++;
+		if (this._loadedImages >= this._items.length) {
+			this._showImages = true;
+		}
 	}
 
 	_titleChanged(e) {
