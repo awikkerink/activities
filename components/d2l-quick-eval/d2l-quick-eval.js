998c74f45dd7170c86295ce625a63d17e92fcccb
Alex Bedley <alex.bedley@D2L.com>
2020-01-08 17:00:49 -0500

    Add new props to subcomponent to avoid potential merge conflict

--- components/d2l-quick-eval/d2l-quick-eval.js
+++ components/d2l-quick-eval/d2l-quick-eval.js
@@ -60,16 +60,27 @@ class D2LQuickEval extends
 					<h1 class="d2l-quick-eval-header-with-toggle" hidden$="[[!activitiesViewEnabled]]">[[headerText]]</h1>
 					<h1 class="d2l-quick-eval-header" hidden$="[[activitiesViewEnabled]]">[[headerText]]</h1>
 				</template>
-				<d2l-quick-eval-ellipsis-menu href="[[dismissedActivitiesHref]]" token="[[token]]" hidden$="[[!dismissEnabled]]"></d2l-quick-eval-ellipsis-menu>
+				<d2l-quick-eval-ellipsis-menu href="[[dismissedActivitiesHref]]" token="[[token]]" hidden$="[[!dismissEnabled]]" course-level="[[courseLevel]]"></d2l-quick-eval-ellipsis-menu>
 			</div>
 			<d2l-quick-eval-view-toggle current-selected="[[toggleState]]" toggle-href="[[toggleHref]]" hidden$="[[!activitiesViewEnabled]]" on-d2l-quick-eval-view-toggle-changed="_toggleView"></d2l-quick-eval-view-toggle>
-			<d2l-quick-eval-submissions href="[[_lazySubmissionsHref]]" token="[[token]]" logging-endpoint="[[loggingEndpoint]]" data-telemetry-endpoint="[[dataTelemetryEndpoint]]" hidden$="[[_displayActivities(toggleState, activitiesViewEnabled)]]" master-teacher="[[masterTeacher]]" returning-to-quick-eval="[[returningToQuickEval]]"></d2l-quick-eval-submissions>
-			<d2l-quick-eval-activities 
+			<d2l-quick-eval-submissions
+				href="[[_lazySubmissionsHref]]"
+				token="[[token]]"
+				logging-endpoint="[[loggingEndpoint]]"
+				data-telemetry-endpoint="[[dataTelemetryEndpoint]]"
+				hidden$="[[_displayActivities(toggleState, activitiesViewEnabled)]]"
+				master-teacher="[[masterTeacher]]"
+				returning-to-quick-eval="[[returningToQuickEval]]"
+				course-level="[[courseLevel]]"
+				submission-filters="[[submissionFilters]]"></d2l-quick-eval-submissions>
+			<d2l-quick-eval-activities
 				href="[[_lazyActivitiesHref]]"
 				token="[[token]]"
 				logging-endpoint="[[loggingEndpoint]]"
 				hidden$="[[!_displayActivities(toggleState, activitiesViewEnabled)]]"
-				dismiss-enabled="[[dismissEnabled]]"></d2l-quick-eval-activities>
+				dismiss-enabled="[[dismissEnabled]]"
+				course-level="[[courseLevel]]"
+				activity-filters="[[activityFilters]]"></d2l-quick-eval-activities>
 		`;
 	}
 
@@ -138,10 +149,12 @@ class D2LQuickEval extends
 				value: false
 			},
 			submissionFilters: {
-				type: Array
+				type: Array,
+				value: []
 			},
 			activityFilters: {
-				type: Array
+				type: Array,
+				value: []
 			}
 		};
 	}
