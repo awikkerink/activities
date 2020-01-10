16694c0473635c30f17a5e671e8f216d1c3b5b54
Dylan Mackenzie <Dylan.MacKenzie@D2L.com>
2020-01-10 11:59:28 -0500

    US112483 - Removing unnecessary imports and cleaning up indentation

--- components/d2l-quick-eval/d2l-quick-eval-no-submissions-text.js
+++ components/d2l-quick-eval/d2l-quick-eval-no-submissions-text.js
@@ -1,21 +1,8 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 import {QuickEvalLocalize} from './QuickEvalLocalize.js';
 import {QuickEvalLogging} from './QuickEvalLogging.js';
-import 'd2l-alert/d2l-alert.js';
 import 'd2l-typography/d2l-typography-shared-styles.js';
 import 'd2l-table/d2l-table.js';
-import 'd2l-button/d2l-button.js';
-import 'd2l-offscreen/d2l-offscreen.js';
-import 'd2l-polymer-behaviors/d2l-dom-focus.js';
-import 'd2l-polymer-behaviors/d2l-id.js';
-import 'd2l-link/d2l-link.js';
-import 'd2l-users/components/d2l-profile-image.js';
-import '../d2l-activity-name/d2l-activity-name.js';
-import '../d2l-activity-evaluation-icon/d2l-activity-evaluation-icon-base.js';
-import './d2l-quick-eval-no-submissions-image.js';
-import './d2l-quick-eval-no-criteria-results-image.js';
-import './d2l-quick-eval-submissions-skeleton.js';
-import 'd2l-loading-spinner/d2l-loading-spinner.js';
 
 /**
  * @customElement
@@ -31,15 +18,15 @@ class D2LQuickEvalNoSubmissionsText extends QuickEvalLogging(QuickEvalLocalize(P
 					margin: 0;
 				}
 			</style>
-            <h2 class="d2l-quick-eval-no-submissions-heading">[[localize('caughtUp')]]</h2>
-            <template is="dom-if" if="[[courseLevel]]">
-                <p class="d2l-body-standard">[[localize('noSubmissionsCourseLevel')]]</p>
-                <p class="d2l-body-standard">[[localize('checkBackOftenCourseLevel')]]</p>
-            </template>
-            <template is="dom-if" if="[[!courseLevel]]">
-                <p class="d2l-body-standard">[[localize('noSubmissions')]]</p>
-                <p class="d2l-body-standard">[[localize('checkBackOften')]]</p>
-            </template>
+			<h2 class="d2l-quick-eval-no-submissions-heading">[[localize('caughtUp')]]</h2>
+			<template is="dom-if" if="[[courseLevel]]">
+				<p class="d2l-body-standard">[[localize('noSubmissionsCourseLevel')]]</p>
+				<p class="d2l-body-standard">[[localize('checkBackOftenCourseLevel')]]</p>
+			</template>
+			<template is="dom-if" if="[[!courseLevel]]">
+				<p class="d2l-body-standard">[[localize('noSubmissions')]]</p>
+				<p class="d2l-body-standard">[[localize('checkBackOften')]]</p>
+			</template>
 		`;
 
 		quickEvalNoSubmissionsTextTemplate.setAttribute('strip-whitespace', 'strip-whitespace');
