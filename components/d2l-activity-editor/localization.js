c596666d9ea776f3430831aaab4981b1e7b0408c
Margaret Dulat <Margaret.Dulat@D2L.com>
2020-01-28 15:43:44 -0800

    translate all supported languages

--- components/d2l-activity-editor/localization.js
+++ components/d2l-activity-editor/localization.js
@@ -4,15 +4,9 @@ export async function getLocalizeResources(langs, importMetaUrl) {
 	const imports = [];
 	let supportedLanguage;
 	for (const language of langs.reverse()) {
-		switch (language) {
-			case 'en':
-				supportedLanguage = 'en';
-				imports.push(import(resolveUrl('./lang/en.js', importMetaUrl)));
-				break;
-			case 'fr':
-				supportedLanguage = 'fr';
-				imports.push(import(resolveUrl('./lang/fr.js', importMetaUrl)));
-				break;
+		if (['en', 'ar', 'de', 'es', 'fr', 'ja', 'ko', 'nl', 'pt', 'sv', 'tr', 'zh', 'zh-tw'].includes(language)) {
+			supportedLanguage = language;
+			imports.push(import(resolveUrl(`./lang/${language}.js`, importMetaUrl)));
 		}
 	}
 
