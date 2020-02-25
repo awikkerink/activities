3989e53fe815926407a868698d0c932fd3c6668e
BrightspaceGitHubReader <brightspacegithubreader@d2l.com>
2020-02-25 11:23:45 -0500

    demo

--- components/d2l-evaluation-page/d2l-evaluation-page.js
+++ components/d2l-evaluation-page/d2l-evaluation-page.js
@@ -1,5 +1,7 @@
 import '@brightspace-ui/core/templates/primary-secondary/primary-secondary.js';
+import './consistent-evaluation-html-editor.js';
 import { css, html, LitElement } from 'lit-element/lit-element.js';
+import { getUniqueId } from '@brightspace-ui/core/helpers/uniqueId';
 
 class EvaluationPage extends LitElement {
 
@@ -27,6 +29,17 @@ class EvaluationPage extends LitElement {
 		`;
 	}
 
+	_resolveUrl() {
+		return `${import.meta.url}/../../node_modules/d2l-html-editor/`;
+	}
+
+	constructor() {
+		super()
+		this.richTextEditorDisabled = false;
+		this._richTextEditorConfig = {};
+		this._htmlEditorUniqueId = `htmleditor-${getUniqueId()}`;
+	}
+
 	render() {
 		return html`
 			<d2l-template-primary-secondary>
@@ -35,12 +48,27 @@ class EvaluationPage extends LitElement {
 					id="d2l-evaluation-page-iframe"
 					src="${this.leftPanelUrl}">
 				</iframe>
-				<button
-					slot="secondary"
-					onclick="SendMessageFromParentToIframe()">Button in parent
-				</button>
+				<div slot="secondary">
+					<button
+						onclick="SendMessageFromParentToIframe()">Button in parent
+					</button>
+					<d2l-consistent-evaluation-html-editor
+						@d2l-request-provider="${this._onRequestProvider}"
+						value="This is the value"
+						.richtextEditorConfig="${this._richTextEditorConfig}"
+						@html-editor-demo-change="${this._saveInstructionsOnChange}"
+						ariaLabel="aria label"
+						?disabled="${this.richTextEditorDisabled}"
+					></d2l-consistent-evaluation-html-editor>
+					<p>hello</p>
+				</div>
+				
 			</d2l-template-primary-secondary>
 		`;
+	}
+
+	_saveInstructionsOnChange() {
+		console.log('save on change');
 	}
 }
 customElements.define('d2l-evaluation-page', EvaluationPage);
