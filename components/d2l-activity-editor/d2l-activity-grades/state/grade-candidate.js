28a5d5b2337d16b5869a8e2b46f95f846af974a7
Vincent Wong <vincent.wong@d2l.com>
2020-03-04 10:36:12 -0800

    Use GradeCategoryEntity

--- components/d2l-activity-editor/d2l-activity-grades/state/grade-candidate.js
+++ components/d2l-activity-editor/d2l-activity-grades/state/grade-candidate.js
@@ -1,6 +1,7 @@
 import { action, configure as configureMobx, decorate, observable } from 'mobx';
 import { fetchEntity } from '../../state/fetch-entity.js';
 import { GradeCandidateEntity } from 'siren-sdk/src/activities/GradeCandidateEntity.js';
+import { GradeCategoryEntity } from 'siren-sdk/src/activities/GradeCategoryEntity.js';
 import { GradeEntity } from 'siren-sdk/src/activities/GradeEntity.js';
 
 configureMobx({ enforceActions: 'observed' });
@@ -14,10 +15,15 @@ export class GradeCandidate {
 
 	async fetch() {
 		const href = this.gradeCandidateEntity.href();
-		const gradeSirenEntity = await fetchEntity(href, this.token);
-		if (gradeSirenEntity) {
-			const gradeEntity = new GradeEntity(gradeSirenEntity, this.token, { remove: () => { } });
-			await this.load(gradeEntity);
+		const sirenEntity = await fetchEntity(href, this.token);
+		if (sirenEntity) {
+			let entity;
+			if (this.gradeCandidateEntity.isCategory()) {
+				entity = new GradeCategoryEntity(sirenEntity, this.token, { remove: () => { } });
+			} else {
+				entity = new GradeEntity(sirenEntity, this.token, { remove: () => { } });
+			}
+			await this.load(entity);
 		}
 		return this;
 	}
@@ -31,10 +37,11 @@ export class GradeCandidate {
 	async load(entity) {
 		this._entity = entity;
 		this.name = entity.name();
-		this.baseWeight = entity.baseWeight();
-		this.maxPoints = entity.maxPoints();
-
 		this.isCategory = this.gradeCandidateEntity.isCategory();
+		if (!this.isCategory) {
+			this.baseWeight = entity.baseWeight();
+			this.maxPoints = entity.maxPoints();
+		}
 		const gradeCandidatePromises = this.gradeCandidateEntity.getGradeCandidates().map(gc => {
 			const gradeCandidateEntity = new GradeCandidateEntity(gc, this.token, { remove: () => { }});
 			return this.fetchGradeCandidate(gradeCandidateEntity);
