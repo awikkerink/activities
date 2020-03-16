0d67b08a9e037749a86236f681266d65e1f4ecd1
Spandana Baruah <Spandana.Baruah@D2L.com>
2020-03-16 12:14:08 -0700

    resize availability dates and conditions summary font size

--- components/d2l-activity-editor/d2l-activity-assignment-editor/d2l-activity-assignment-availability-editor.js
+++ components/d2l-activity-editor/d2l-activity-assignment-editor/d2l-activity-assignment-availability-editor.js
@@ -2,6 +2,7 @@ import '../d2l-activity-availability-dates-summary.js';
 import '../d2l-activity-availability-dates-editor.js';
 import '../d2l-activity-usage-conditions-editor.js';
 import '@brightspace-ui-labs/accordion/accordion-collapse.js';
+import { bodySmallStyles } from '@brightspace-ui/core/components/typography/styles.js';
 import { css, html } from 'lit-element/lit-element.js';
 import { heading3Styles, heading4Styles } from '@brightspace-ui/core/components/typography/styles.js';
 import { summarizerHeaderStyles, summarizerSummaryStyles } from './activity-summarizer-styles.js';
@@ -26,6 +27,7 @@ class ActivityAssignmentAvailabilityEditor extends LocalizeMixin(ActivityEditorM
 	static get styles() {
 
 		return [
+			bodySmallStyles,
 			heading3Styles,
 			heading4Styles,
 			css`
