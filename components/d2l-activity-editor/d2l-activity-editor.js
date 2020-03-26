9036d321c211e6562feb3d8df3b8425433f3e8de
Emil Furniss <emil.furniss@d2l.com>
2020-03-26 12:30:39 -0700

    lint

--- components/d2l-activity-editor/d2l-activity-editor.js
+++ components/d2l-activity-editor/d2l-activity-editor.js
@@ -1,8 +1,8 @@
+import { AsyncContainerMixin, asyncStates } from '@brightspace-ui/core/mixins/async-container/async-container-mixin.js';
 import { css, html, LitElement } from 'lit-element/lit-element.js';
 import { ActivityEditorMixin } from './mixins/d2l-activity-editor-mixin.js';
 import { getLocalizeResources } from './localization';
 import { LocalizeMixin } from '@brightspace-ui/core/mixins/localize-mixin.js';
-import { AsyncContainerMixin, asyncStates } from '@brightspace-ui/core/mixins/async-container/async-container-mixin.js';
 import { shared as store } from './state/activity-store.js';
 
 class ActivityEditor extends AsyncContainerMixin(ActivityEditorMixin(LocalizeMixin(LitElement))) {
