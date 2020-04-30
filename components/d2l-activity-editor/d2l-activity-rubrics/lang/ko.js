48637c3e83994fa0d3da5234da867b67af5cef45
Emil Furniss <emil.furniss@d2l.com>
2020-04-30 11:31:33 -0700

    fix ko plural translations

--- components/d2l-activity-editor/d2l-activity-rubrics/lang/ko.js
+++ components/d2l-activity-editor/d2l-activity-rubrics/lang/ko.js
@@ -9,7 +9,7 @@ export default {
 	"btnCancel": "취소", //Button for canceling out of the attach new rubric overlay
 	"txtAddExisting": "기존 항목 추가", //Title for the attach rubrics dialog,
 	"txtNoRubricAdded": "추가된 루브릭 없음", // rubric summary for no rubrics
-	"txtRubricsAdded": "{수, 복수, =1 {루브릭 1개 추가} 기타 {{count}개 루브릭 추가}}", // count of asoociated rubrics
+	"txtRubricsAdded": "{count, plural, =1 {루브릭 1개 추가} other {{count}개 루브릭 추가}}", // count of asoociated rubrics
 	"txtOpenRubricPreview": "루브릭 미리 보기 대화 상자를 엽니다.", //Text for opening rubric preview dialog
 	"txtDeleteRubric": "루브릭 삭제" // Text for deleting rubric icon
 };
