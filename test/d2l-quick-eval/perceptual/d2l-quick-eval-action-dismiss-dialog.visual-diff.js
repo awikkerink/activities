8bb01dd350ddadb901ce98a280421411d784a0ed
Alex Bedley <Alex.Bedley@D2L.com>
2020-05-19 12:37:32 -0400

    Unskip qe vdiff tests

--- test/d2l-quick-eval/perceptual/d2l-quick-eval-action-dismiss-dialog.visual-diff.js
+++ test/d2l-quick-eval/perceptual/d2l-quick-eval-action-dismiss-dialog.visual-diff.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const VisualDiff = require('@brightspace-ui/visual-diff');
 
-describe.skip('d2l-quick-eval-action-dismiss-dialog', function() {
+describe('d2l-quick-eval-action-dismiss-dialog', function() {
 
 	const visualDiff = new VisualDiff('action-dismiss-dialog', __dirname);
 
