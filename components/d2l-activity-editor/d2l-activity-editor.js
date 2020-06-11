941a89ccbd0700ae53f5235b56b80e3261412122
Emil Furniss <emil.furniss@d2l.com>
2020-06-11 09:38:41 -0700

    update backdrop attributes

--- components/d2l-activity-editor/d2l-activity-editor.js
+++ components/d2l-activity-editor/d2l-activity-editor.js
@@ -93,7 +93,12 @@ class ActivityEditor extends ActivityEditorTelemetryMixin(AsyncContainerMixin(Ac
 			<div id="editor-container" ?hidden="${this.asyncState !== asyncStates.complete}">
 				<slot name="editor"></slot>
 			</div>
-			<d2l-backdrop for-target="editor-container" ?shown="${this._backdropShown}" cut-out slow-transition></d2l-backdrop>
+			<d2l-backdrop
+				for-target="editor-container"
+				?shown="${this._backdropShown}"
+				no-animate-hide
+				slow-transition>
+			</d2l-backdrop>
 		`;
 	}
 }
