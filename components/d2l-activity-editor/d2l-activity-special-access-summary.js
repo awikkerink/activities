7e9872f8742602e21cf91086fee2edfd20e6a8dc
Spandana Baruah <Spandana.Baruah@D2L.com>
2020-07-09 15:56:05 -0700

    US117524 add special access userCount summary

--- components/d2l-activity-editor/d2l-activity-special-access-summary.js
+++ components/d2l-activity-editor/d2l-activity-special-access-summary.js
@@ -1,9 +1,15 @@
 import { ActivityEditorMixin } from './mixins/d2l-activity-editor-mixin.js';
+import { getLocalizeResources } from './localization';
 import { html } from 'lit-element/lit-element';
+import { LocalizeMixin } from '@brightspace-ui/core/mixins/localize-mixin.js';
 import { MobxLitElement } from '@adobe/lit-mobx';
 import { shared as store } from './state/activity-store.js';
 
-class ActivitySpecialAccessSummary extends ActivityEditorMixin(MobxLitElement) {
+class ActivitySpecialAccessSummary extends ActivityEditorMixin(LocalizeMixin(MobxLitElement)) {
+
+	static async getLocalizeResources(langs) {
+		return getLocalizeResources(langs, import.meta.url);
+	}
 
 	constructor() {
 		super(store);
@@ -12,7 +18,7 @@ class ActivitySpecialAccessSummary extends ActivityEditorMixin(MobxLitElement) {
 	_renderSummary(specialAccess) {
 		const { userCount } = specialAccess;
 
-		if (userCount <= 0) {
+		if (userCount === 0) {
 			return html``;
 		} else {
 			return html`${this.localize('txtNumSpecialAccess', { userCount })}`;
