f5599a7aa87413cece9a14727eef5d6575c98fd8
Spandana Baruah <Spandana.Baruah@D2L.com>
2020-07-08 17:10:08 -0700

    code changes as of now

---
+++ components/d2l-activity-editor/d2l-activity-special-access-summary.js
@@ -0,0 +1,36 @@
+import { ActivityEditorMixin } from './mixins/d2l-activity-editor-mixin.js';
+import { html } from 'lit-element/lit-element';
+import { MobxLitElement } from '@adobe/lit-mobx';
+import { shared as store } from './state/activity-store.js';
+
+class ActivitySpecialAccessSummary extends ActivityEditorMixin(MobxLitElement) {
+
+	constructor() {
+		super(store);
+	}
+
+	_renderSummary(specialAccess) {
+		const { userCount } = specialAccess;
+
+		if (userCount <= 0) {
+			return html``;
+		} else {
+			return html`${this.localize('txtNumSpecialAccess', { userCount })}`;
+		}
+	}
+
+	render() {
+
+		const entity = store.get(this.href);
+		if (!entity || !entity.specialAccess) {
+			return html``;
+		} else {
+			return html`${this._renderSummary(entity.specialAccess)}`;
+		}
+	}
+}
+
+customElements.define(
+	'd2l-activity-special-access-summary',
+	ActivitySpecialAccessSummary
+);
