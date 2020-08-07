eea6c24b31cea00ccfa20906e264fac3dce9a22f
Steven Ao <stevenao@users.noreply.github.com>
2020-08-07 10:49:23 -0700

    Submission notification field validation (#1017)

--- components/d2l-activity-editor/d2l-activity-assignment-editor/d2l-activity-assignment-editor-submission-and-completion.js
+++ components/d2l-activity-editor/d2l-activity-assignment-editor/d2l-activity-assignment-editor-submission-and-completion.js
@@ -9,6 +9,7 @@ import { bodyCompactStyles, bodySmallStyles, heading3Styles, labelStyles } from 
 import { css, html } from 'lit-element/lit-element.js';
 import { summarizerHeaderStyles, summarizerSummaryStyles } from './activity-summarizer-styles.js';
 import { ActivityEditorMixin } from '../mixins/d2l-activity-editor-mixin.js';
+import { ErrorHandlingMixin } from '../error-handling-mixin.js';
 import { LocalizeActivityAssignmentEditorMixin } from './mixins/d2l-activity-assignment-lang-mixin.js';
 import { MobxLitElement } from '@adobe/lit-mobx';
 import { radioStyles } from '@brightspace-ui/core/components/inputs/input-radio-styles.js';
@@ -16,11 +17,12 @@ import { RtlMixin } from '@brightspace-ui/core/mixins/rtl-mixin.js';
 import { selectStyles } from '@brightspace-ui/core/components/inputs/input-select-styles.js';
 import { shared as store } from './state/assignment-store.js';
 
-class ActivityAssignmentSubmissionAndCompletionEditor extends ActivityEditorFeaturesMixin(ActivityEditorMixin(RtlMixin(LocalizeActivityAssignmentEditorMixin(MobxLitElement)))) {
+class ActivityAssignmentSubmissionAndCompletionEditor extends ActivityEditorFeaturesMixin(ActivityEditorMixin(ErrorHandlingMixin(RtlMixin(LocalizeActivityAssignmentEditorMixin(MobxLitElement))))) {
 
 	static get properties() {
 
 		return {
+			_notificationEmailError: { type: String },
 			href: { type: String },
 			token: { type: Object },
 			_m4EmailNotificationEnabled: { type: Boolean }
@@ -239,13 +241,39 @@ class ActivityAssignmentSubmissionAndCompletionEditor extends ActivityEditorFeat
 				maxlength="1024"
 				?disabled="${!assignment.canEditNotificationEmail}"
 				@change="${this._onNotificationEmailChanged}"
-				@blur="${this._onNotificationEmailChanged}"
+				@blur="${this._checkNotificationEmail}"
+				aria-invalid="${this._notificationEmailError ? 'true' : ''}"
+				skip-alert
 				novalidate
 			></d2l-input-text>
+			${this._getNotificationEmailTooltip()}
 		</div>
 	`;
 	}
+	_getNotificationEmailTooltip() {
+		if (this._notificationEmailError) {
+			return html `
+				<d2l-tooltip id="notification-email-tooltip" for="notification-email" state="error" align="start" offset="10">
+					${this._notificationEmailError}
+				</d2l-tooltip>
+			`;
+		}
+	}
+	_checkNotificationEmail(e) {
+		const errorProperty = '_notificationEmailError';
+		const invalidNotificationEmailErrorLangterm = 'invalidNotificationEmailError';
+		const tooltipId = 'notification-email-tooltip';
 
+		const notificationEmail = e.target.value;
+		const isEmpty = (notificationEmail || '').length === 0;
+
+		const matches = /^(\s?[^\s,]+@[^\s,]+\.[^\s,]+\s?,)*(\s?[^\s,]+@[^\s,]+\.[^\s,]+)$/.exec(notificationEmail);
+		if (!isEmpty && matches === null) {
+			this.setError(errorProperty, invalidNotificationEmailErrorLangterm, tooltipId);
+		} else {
+			this.clearError(errorProperty);
+		}
+	}
 	_onNotificationEmailChanged(e) {
 		const assignment = store.getAssignment(this.href);
 		const data = e.target.value;
