0e35cf0ed1bf7b673ccba5b9412e294844177006
Andrew Wong <Andrew.Wong@D2L.com>
2020-09-04 16:04:22 -0700

    Remove duplicate styles

--- components/d2l-activity-editor/mixins/d2l-skeletize-mixin.js
+++ components/d2l-activity-editor/mixins/d2l-skeletize-mixin.js
@@ -34,33 +34,6 @@ export const SkeletizeMixin = superclass => class extends superclass {
 			color: transparent;
 			position: relative;
 		}
-
-		@keyframes loadingPulse {
-			0% { background-color: var(--d2l-color-sylvite); }
-			50% { background: var(--d2l-color-regolith); }
-			75% { background: var(--d2l-color-sylvite); }
-			100% { background: var(--d2l-color-sylvite); }
-		}
-
-		.d2l-skeletize::before {
-			animation: loadingPulse 1.8s linear infinite;
-			background-color: var(--d2l-color-sylvite);
-			border-radius: 0.2rem;
-			bottom: 0;
-			content: "";
-			left: 0;
-			position: absolute;
-			right: 0;
-			top: 0;
-			z-index: 2000;
-		}
-
-		.d2l-skeletize {
-			border: none;
-			box-shadow: none;
-			color: transparent;
-			position: relative;
-		}
 		`;
 	}
 
