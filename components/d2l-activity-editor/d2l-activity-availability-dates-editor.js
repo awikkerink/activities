8b6b442283bb7d76025bcc4965f36a278404852e
Allan Kerr <allan.kerr@D2L.com>
2020-09-04 14:31:42 -0400

    Add validation for start and end date inputs

--- components/d2l-activity-editor/d2l-activity-availability-dates-editor.js
+++ components/d2l-activity-editor/d2l-activity-availability-dates-editor.js
@@ -1,4 +1,5 @@
 import '@brightspace-ui/core/components/inputs/input-date-time.js';
+import '@brightspace-ui/core/components/validation/validation-custom.js';
 import { css, html } from 'lit-element/lit-element';
 import { ActivityEditorMixin } from './mixins/d2l-activity-editor-mixin.js';
 import { labelStyles } from '@brightspace-ui/core/components/typography/styles.js';
@@ -30,30 +31,38 @@ class ActivityAvailabilityDatesEditor extends (ActivityEditorMixin(LocalizeActiv
 			return html``;
 		}
 
-		const startDateErrorTerm = this.localize(dates.startDateErrorTerm);
-		const startDateErrorValue = startDateErrorTerm ? startDateErrorTerm : null;
-
-		const endDateErrorTerm = this.localize(dates.endDateErrorTerm);
-		const endDateErrorValue = endDateErrorTerm ? endDateErrorTerm : null;
-
 		return html`
 			<d2l-input-date-time
 				id="start-date-input"
 				label="${this.localize('editor.startDate')}"
 				value="${dates.startDate}"
-				.validationError="${startDateErrorValue}"
-				?invalid="${startDateErrorValue}"
 				@change="${this._onStartDatetimeChanged}">
 			</d2l-input-date-time>
+			<d2l-validation-custom for="start-date-input" @d2l-validation-custom-validate=${this._validateStartDate} failure-text=${this.localize(dates.startDateErrorTerm)}>
+			</d2l-validation-custom>
+
 			<d2l-input-date-time
 				id="end-date-input"
 				label="${this.localize('editor.endDate')}"
 				value="${dates.endDate}"
-				.validationError="${endDateErrorValue}"
-				?invalid="${endDateErrorValue}"
 				@change="${this._onEndDatetimeChanged}">
 			</d2l-input-date-time>
+			<d2l-validation-custom for="end-date-input" @d2l-validation-custom-validate=${this._validateEndDate} failure-text=${this.localize(dates.endDateErrorTerm)}>
+			</d2l-validation-custom>
 		`;
+	}
+
+	updated(properties) {
+		super.updated(properties);
+
+		const startDateInput = this.shadowRoot.querySelector('#start-date-input');
+		if (startDateInput) {
+			startDateInput.requestValidate();
+		}
+		const endDateInput = this.shadowRoot.querySelector('#end-date-input');
+		if (endDateInput) {
+			endDateInput.requestValidate();
+		}
 	}
 
 	_onEndDatetimeChanged(e) {
@@ -63,5 +72,24 @@ class ActivityAvailabilityDatesEditor extends (ActivityEditorMixin(LocalizeActiv
 	_onStartDatetimeChanged(e) {
 		store.get(this.href).dates.setStartDate(e.target.value);
 	}
+
+	_validateEndDate(e) {
+		const entity = store.get(this.href);
+		if (!entity || !entity.dates || !entity.dates.canEditDates) {
+			e.detail.resolve(true);
+			return;
+		}
+		e.detail.resolve(!entity.dates.endDateErrorTerm);
+	}
+
+	_validateStartDate(e) {
+		const entity = store.get(this.href);
+		if (!entity || !entity.dates || !entity.dates.canEditDates) {
+			e.detail.resolve(true);
+			return;
+		}
+		e.detail.resolve(!entity.dates.startDateErrorTerm);
+	}
+
 }
 customElements.define('d2l-activity-availability-dates-editor', ActivityAvailabilityDatesEditor);
