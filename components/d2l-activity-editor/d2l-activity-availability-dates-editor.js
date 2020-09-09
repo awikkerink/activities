6f3a90f3cf499427ffc30599c8d7caf1b02bcbb7
Emil Furniss <32819802+emil-furniss@users.noreply.github.com>
2020-09-09 09:13:08 -0700

    US116271 Use new d2l-input-date-time for FACE due and availability dates (#1067)
    * use new d2l-input-date-time for due and availability dates
    * move d2l-acivity-availability-dates-editor tests to karma
    * use d2l-validation-custom for validation logic

--- components/d2l-activity-editor/d2l-activity-availability-dates-editor.js
+++ components/d2l-activity-editor/d2l-activity-availability-dates-editor.js
@@ -1,4 +1,5 @@
-import 'd2l-datetime-picker/d2l-datetime-picker';
+import '@brightspace-ui/core/components/inputs/input-date-time.js';
+import '@brightspace-ui/core/components/validation/validation-custom.js';
 import { css, html } from 'lit-element/lit-element';
 import { ActivityEditorMixin } from './mixins/d2l-activity-editor-mixin.js';
 import { labelStyles } from '@brightspace-ui/core/components/typography/styles.js';
@@ -7,106 +8,138 @@ import { MobxLitElement } from '@adobe/lit-mobx';
 import { shared as store } from './state/activity-store.js';
 
 class ActivityAvailabilityDatesEditor extends (ActivityEditorMixin(LocalizeActivityEditorMixin(MobxLitElement))) {
+
 	static get styles() {
 		return [labelStyles, css`
-			:host {
-				display: block;
-			}
 			:host([hidden]) {
 				display: none;
 			}
-			#startdate-container,
-			#enddate-container {
-				min-height: 62px; /* Hack to force a consistent the height for the old datetime picker. Can hopefully be removed when the new picker is used. */
+			#start-date-input {
+				padding-bottom: 20px;
 			}
 		`];
 	}
 
 	constructor() {
-		super();
-		this._overrides = document.documentElement.dataset.intlOverrides || '{}';
+		super(store);
 	}
 
 	render() {
-		const entity = store.get(this.href);
-		const dates = entity ? entity.dates : null;
-		let canEditDates, startDate, endDate, startDateErrorTerm, endDateErrorTerm;
+		const {
+			canEditDates,
+			startDate,
+			endDate,
+			startDateError,
+			endDateError
+		} = this._getDateValues();
 
-		if (!dates) {
-			canEditDates = false;
-			startDate = null;
-			endDate = null;
-			startDateErrorTerm = null;
-			endDateErrorTerm = null;
-		} else {
-			canEditDates = dates.canEditDates;
-			startDate = dates.startDate;
-			endDate = dates.endDate;
-			startDateErrorTerm = this.localize(dates.startDateErrorTerm);
-			endDateErrorTerm = this.localize(dates.endDateErrorTerm);
+		if (!canEditDates) {
+			return html``;
 		}
 
 		return html`
-			<label class="d2l-label-text" ?hidden=${!canEditDates}>${this.localize('editor.startDate')}</label>
-			<div id="startdate-container" ?hidden=${!canEditDates}>
-				<d2l-datetime-picker
-					hide-label
-					name="startDate"
-					id="startDate"
-					date-label="${this.localize('editor.startDate')}"
-					time-label="${this.localize('editor.startTime')}"
-					datetime="${startDate}"
-					overrides="${this._overrides}"
-					placeholder="${this.localize('editor.noStartDate')}"
-					aria-invalid="${startDateErrorTerm ? 'true' : 'false'}"
-					invalid="${startDateErrorTerm}"
-					tooltip-red
-					boundary="{&quot;below&quot;:240}"
-					@d2l-datetime-picker-datetime-changed="${this._onStartDatetimePickerDatetimeChanged}"
-					@d2l-datetime-picker-datetime-cleared="${this._onStartDatetimePickerDatetimeCleared}">
-				</d2l-datetime-picker>
-			</div>
-			<label class="d2l-label-text" ?hidden=${!canEditDates}>${this.localize('editor.endDate')}</label>
-			<div id="enddate-container" ?hidden=${!canEditDates}>
-				<d2l-datetime-picker
-					hide-label
-					name="endDate"
-					id="endDate"
-					date-label="${this.localize('editor.endDate')}"
-					time-label="${this.localize('editor.endTime')}"
-					datetime="${endDate}"
-					overrides="${this._overrides}"
-					placeholder="${this.localize('editor.noEndDate')}"
-					aria-invalid="${endDateErrorTerm ? 'true' : 'false'}"
-					invalid="${endDateErrorTerm}"
-					tooltip-red
-					boundary="{&quot;below&quot;:240}"
-					@d2l-datetime-picker-datetime-changed="${this._onEndDatetimePickerDatetimeChanged}"
-					@d2l-datetime-picker-datetime-cleared="${this._onEndDatetimePickerDatetimeCleared}">
-				</d2l-datetime-picker>
-			</div>
+			<d2l-input-date-time
+				id="start-date-input"
+				label="${this.localize('editor.startDate')}"
+				value="${startDate}"
+				@change="${this._onStartDatetimeChanged}">
+			</d2l-input-date-time>
+			<d2l-validation-custom
+				for="start-date-input"
+				@d2l-validation-custom-validate=${this._validateStartDate}
+				failure-text="${startDateError}">
+			</d2l-validation-custom>
+			<d2l-input-date-time
+				id="end-date-input"
+				label="${this.localize('editor.endDate')}"
+				value="${endDate}"
+				@change="${this._onEndDatetimeChanged}">
+			</d2l-input-date-time>
+			<d2l-validation-custom
+				for="end-date-input"
+				@d2l-validation-custom-validate=${this._validateEndDate}
+				failure-text="${endDateError}">
+			</d2l-validation-custom>
 		`;
 	}
-	updated(changedProperties) {
-		super.updated(changedProperties);
 
-		if ((changedProperties.has('href') || changedProperties.has('token')) &&
-			this.href && this.token) {
-			super._fetch(() => store.fetch(this.href, this.token));
+	updated(properties) {
+		super.updated(properties);
+
+		const startDateInput = this.shadowRoot.querySelector('#start-date-input');
+		if (startDateInput) {
+			startDateInput.requestValidate();
+		}
+		const endDateInput = this.shadowRoot.querySelector('#end-date-input');
+		if (endDateInput) {
+			endDateInput.requestValidate();
 		}
 	}
-	_onEndDatetimePickerDatetimeChanged(e) {
-		store.get(this.href).dates.setEndDate(e.detail.toISOString());
-	}
-	_onEndDatetimePickerDatetimeCleared() {
-		store.get(this.href).dates.setEndDate('');
-	}
-	_onStartDatetimePickerDatetimeChanged(e) {
-		store.get(this.href).dates.setStartDate(e.detail.toISOString());
-	}
-	_onStartDatetimePickerDatetimeCleared() {
-		store.get(this.href).dates.setStartDate('');
+
+	_getDateValues() {
+		const datesEntity = {
+			canEditDates: true,
+			startDate: null,
+			endDate: null,
+			startDateError: null,
+			endDateError: null
+		};
+
+		const entity = store.get(this.href);
+		if (!entity || !entity.dates) {
+			return datesEntity;
+		}
+
+		const dates = entity.dates;
+
+		if (!dates.canEditDates) {
+			datesEntity.canEditDates = false;
+			return datesEntity;
+		}
+
+		if (dates.startDateErrorTerm) {
+			datesEntity.startDateError = this.localize(dates.startDateErrorTerm);
+		}
+
+		if (dates.endDateErrorTerm) {
+			datesEntity.endDateError = this.localize(dates.endDateErrorTerm);
+		}
+
+		if (dates.startDate) {
+			datesEntity.startDate = dates.startDate;
+		}
+
+		if (dates.endDate) {
+			datesEntity.endDate = dates.endDate;
+		}
+
+		return datesEntity;
 	}
 
+	_onEndDatetimeChanged(e) {
+		store.get(this.href).dates.setEndDate(e.target.value);
+	}
+
+	_onStartDatetimeChanged(e) {
+		store.get(this.href).dates.setStartDate(e.target.value);
+	}
+
+	_validateEndDate(e) {
+		const entity = store.get(this.href);
+		if (!entity || !entity.dates || !entity.dates.canEditDates) {
+			e.detail.resolve(true);
+			return;
+		}
+		e.detail.resolve(!entity.dates.endDateErrorTerm);
+	}
+
+	_validateStartDate(e) {
+		const entity = store.get(this.href);
+		if (!entity || !entity.dates || !entity.dates.canEditDates) {
+			e.detail.resolve(true);
+			return;
+		}
+		e.detail.resolve(!entity.dates.startDateErrorTerm);
+	}
 }
 customElements.define('d2l-activity-availability-dates-editor', ActivityAvailabilityDatesEditor);
---
+++ test/d2l-activity-editor/d2l-activity-availability-dates-editor.test.js
@@ -0,0 +1,119 @@
+import '../../components/d2l-activity-editor/d2l-activity-availability-dates-editor';
+import { elementUpdated, expect, fixture, html, nextFrame } from '@open-wc/testing';
+import { ActivityDates } from '../../components/d2l-activity-editor/state/activity-dates';
+import { ActivityUsage } from '../../components/d2l-activity-editor/state/activity-usage.js';
+import { shared as store } from '../../components/d2l-activity-editor/state/activity-store.js';
+
+describe('d2l-activity-availability-dates-editor', function() {
+
+	let el, href, activity, dates, startDateInput, endDateInput;
+
+	beforeEach(async() => {
+		dates = new ActivityDates({
+			canEditDates: () => true,
+			startDate: () => '2020-02-24T04:59:00.000Z',
+			dueDate: () => '2020-02-25T04:59:00.000Z',
+			endDate: () => '2020-02-26T04:59:00.000Z'
+		});
+
+		href = 'http://activity/1';
+		activity = new ActivityUsage(href, 'token');
+		activity.setDates(dates);
+		store.put(href, activity);
+
+		el = await fixture(html`
+			<d2l-activity-availability-dates-editor href=${href} token="token"></d2l-activity-availability-dates-editor>
+		`);
+
+		startDateInput = el.shadowRoot.querySelector('#start-date-input');
+		endDateInput = el.shadowRoot.querySelector('#end-date-input');
+	});
+
+	afterEach(() => {
+		store.clear();
+	});
+
+	it('passes accessibility test', async() => {
+		await expect(el).to.be.accessible();
+	});
+
+	describe('inputs visible', function() {
+		it('renders both availability date inputs when enabled', async() => {
+			expect(startDateInput).to.exist;
+			expect(endDateInput).to.exist;
+		});
+	});
+
+	describe('inputs hidden', function() {
+		beforeEach(async() => {
+			dates.setCanEditDates(false);
+			await elementUpdated(el);
+
+			startDateInput = el.shadowRoot.querySelector('#start-date-input');
+			endDateInput = el.shadowRoot.querySelector('#end-date-input');
+		});
+
+		it('hides both availability date inputs if not allowed to edit', async() => {
+			expect(startDateInput).to.not.exist;
+			expect(endDateInput).to.not.exist;
+		});
+	});
+
+	describe('dates', function() {
+		it('displays correct dates', async() => {
+			expect(startDateInput).to.have.attr('value', '2020-02-24T04:59:00.000Z');
+			expect(endDateInput).to.have.attr('value', '2020-02-26T04:59:00.000Z');
+		});
+
+		it('updates date values on change', async() => {
+			dates.setStartDate('2020-02-25T04:59:00.000Z');
+			dates.setEndDate('2020-02-27T04:59:00.000Z');
+			await elementUpdated(el);
+
+			expect(startDateInput).to.have.attr('value', '2020-02-25T04:59:00.000Z');
+			expect(endDateInput).to.have.attr('value', '2020-02-27T04:59:00.000Z');
+		});
+	});
+
+	describe('invalid state', function() {
+		it('sets start date to be invalid for given error', async() => {
+			dates.setErrorLangTerms('start-after-due-date-error');
+			await nextFrame();
+			await elementUpdated(el);
+
+			expect(startDateInput.invalid).to.be.true;
+			expect(endDateInput.invalid).to.be.false;
+		});
+		it('sets end date to be invalid for given error', async() => {
+			dates.setErrorLangTerms('end-before-due-date-error');
+			await nextFrame();
+			await elementUpdated(el);
+
+			expect(startDateInput.invalid).to.be.false;
+			expect(endDateInput.invalid).to.be.true;
+		});
+		it('sets start and end date to be invalid for given error', async() => {
+			dates.setErrorLangTerms('end-before-start-due-date-error');
+			await nextFrame();
+			await elementUpdated(el);
+
+			expect(startDateInput.invalid).to.be.true;
+			expect(endDateInput.invalid).to.be.true;
+		});
+		it('clears invalid state', async() => {
+			dates.setErrorLangTerms('end-before-start-due-date-error');
+			await nextFrame();
+			await elementUpdated(el);
+
+			expect(startDateInput.invalid).to.be.true;
+			expect(endDateInput.invalid).to.be.true;
+
+			dates.setErrorLangTerms();
+			await nextFrame();
+			await elementUpdated(el);
+
+			expect(startDateInput.invalid).to.be.false;
+			expect(endDateInput.invalid).to.be.false;
+		});
+	});
+});
