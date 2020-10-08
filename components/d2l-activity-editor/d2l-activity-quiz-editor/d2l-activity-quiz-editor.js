583f71804bf8a608d03cb4bae72954f74c5154a8
Ronan Fegan <rfegan13@gmail.com>
2020-10-08 13:34:52 -0700

    Updated mixins used by quiz-editor to match assignments-editor

--- components/d2l-activity-editor/d2l-activity-quiz-editor/d2l-activity-quiz-editor.js
+++ components/d2l-activity-editor/d2l-activity-quiz-editor/d2l-activity-quiz-editor.js
@@ -1,8 +1,10 @@
 import './d2l-activity-quiz-editor-detail.js';
-import { html, LitElement } from 'lit-element/lit-element.js';
-import { EntityMixinLit } from 'siren-sdk/src/mixin/entity-mixin-lit.js';
-
-class QuizEditor extends EntityMixinLit(LitElement) {
+import { ActivityEditorMixin } from '../mixins/d2l-activity-editor-mixin.js';
+import { AsyncContainerMixin } from '@brightspace-ui/core/mixins/async-container/async-container-mixin.js';
+import { html } from 'lit-element/lit-element.js';
+import { MobxLitElement } from '@adobe/lit-mobx';
+import { RtlMixin } from '@brightspace-ui/core/mixins/rtl-mixin.js';
+class QuizEditor extends AsyncContainerMixin(RtlMixin(ActivityEditorMixin(MobxLitElement))) {
 
 	static get properties() {
 		return {
