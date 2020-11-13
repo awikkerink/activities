6cd8222e7e0a1aeaaadc9e3d7568d97c29e4509f
Ronan Fegan <rfegan13@gmail.com>
2020-11-13 13:31:09 -0800

    Added attempts and completion accordion

---
+++ components/d2l-activity-editor/d2l-activity-quiz-editor/d2l-activity-quiz-attempts-and-completion-editor.js
@@ -0,0 +1,59 @@
+import '../d2l-activity-accordion-collapse.js';
+import '../d2l-activity-availability-dates-editor.js';
+import '../d2l-activity-availability-dates-summary.js';
+import './d2l-activity-quiz-hints-editor.js';
+import './d2l-activity-quiz-hints-summary.js';
+import { accordionStyles } from '../styles/accordion-styles';
+import { ActivityEditorFeaturesMixin } from '../mixins/d2l-activity-editor-features-mixin.js';
+import { ActivityEditorMixin } from '../mixins/d2l-activity-editor-mixin.js';
+import { AsyncContainerMixin } from '@brightspace-ui/core/mixins/async-container/async-container-mixin.js';
+import { html } from 'lit-element/lit-element.js';
+import { LocalizeActivityQuizEditorMixin } from './mixins/d2l-activity-quiz-lang-mixin';
+import { MobxLitElement } from '@adobe/lit-mobx';
+import { SkeletonMixin } from '@brightspace-ui/core/components/skeleton/skeleton-mixin.js';
+
+class ActivityQuizAttemptsAndCompletionEditor extends AsyncContainerMixin(LocalizeActivityQuizEditorMixin(SkeletonMixin(ActivityEditorFeaturesMixin(ActivityEditorMixin(MobxLitElement))))) {
+
+	static get properties() {
+
+		return {
+			href: { type: String },
+			token: { type: Object }
+		};
+	}
+
+	static get styles() {
+
+		return [
+			super.styles,
+			accordionStyles
+		];
+	}
+
+	connectedCallback() {
+		super.connectedCallback();
+	}
+
+	render() {
+		return html`
+			<d2l-activity-accordion-collapse
+				?has-errors=${this._errorInAccordion()}
+				?skeleton="${this.skeleton}">
+
+				<span slot="header">
+					${this.localize('hdrAttemptsAndCompletion')}
+				</span>
+
+			</d2l-activity-accordion-collapse>
+		`;
+	}
+	// Returns true if any error states relevant to this accordion are set
+	_errorInAccordion() {
+		return false; // Todo: implement error handling
+	}
+}
+
+customElements.define(
+	'd2l-activity-quiz-attempts-and-completion-editor',
+	ActivityQuizAttemptsAndCompletionEditor
+);
