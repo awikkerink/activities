1e02b13ee107e1d4f1bd8e923a09aa4f483e137d
Andrew Wong <Andrew.Wong@D2L.com>
2020-11-23 01:13:29 -0800

    Add langterm

--- components/d2l-activity-editor/d2l-activity-quiz-editor/lang/en.js
+++ components/d2l-activity-editor/d2l-activity-quiz-editor/lang/en.js
@@ -11,5 +11,6 @@ export default {
 	"hintsAllowedSummary": "Hints allowed", // summary text when hints are enabled,
 	"passwordLabel": "Password", // Label for the quiz password text input field
 	"disableRightClickDescription": "Disable right click", // description for disable right click
-	"disableRightClickSummary": "Right clicks disabled" // summary text when right clicks are disabled
+	"disableRightClickSummary": "Right clicks disabled", // summary text when right clicks are disabled
+	"disablePagerAndAlertsDescription": "Block alerts and communication during quiz attempts",
 };
