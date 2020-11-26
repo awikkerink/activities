2543644783496a8b1cd1188bcca570b96b03a04b
Ronan Fegan <rfegan13@gmail.com>
2020-11-26 15:57:17 -0800

    Added lang term for moving backwards checkbox

--- components/d2l-activity-editor/d2l-activity-quiz-editor/lang/en.js
+++ components/d2l-activity-editor/d2l-activity-quiz-editor/lang/en.js
@@ -15,4 +15,5 @@ export default {
 	"disableRightClickSummary": "Right clicks disabled", // summary text when right clicks are disabled
 	"disablePagerAndAlertsDescription": "Block alerts and communication during quiz attempts", // description for disable pager and alerts / block communications during a quiz,
 	"pagingTools": "Paging", // Title for paging tools
+	"preventMovingBackwardsDescription": "Prevent going back to previous pages"
 };
