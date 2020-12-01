3abc74b097a671ca43a16709f7c0db9edffd2a7f
Ronan Fegan <rfegan13@gmail.com>
2020-12-01 11:09:22 -0800

    Updated class names

--- components/d2l-activity-editor/d2l-activity-quiz-divider.js
+++ components/d2l-activity-editor/d2l-activity-quiz-divider.js
@@ -27,7 +27,7 @@ class ActivityQuizDivider extends ActivityEditorMixin(RtlMixin(LocalizeActivityE
 				margin-left: 0;
 				margin-right: -20px;
 			}
-			.d2l-container {
+			.d2l-divider-container {
 				width: inherit;
 				display: flex;
 				justify-content: space-between;
@@ -40,7 +40,7 @@ class ActivityQuizDivider extends ActivityEditorMixin(RtlMixin(LocalizeActivityE
 	render() {
 		return html`
 			<div class="d2l-activity-divider">
-				<div class="d2l-container d2l-primary-panel">
+				<div class="d2l-divider-container">
 					<slot name="header"></slot>
 					<slot name="action"></slot>
 				</div>
