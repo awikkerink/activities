d560494383504cef8885757bfd3af515fb311911
Margaret Dulat <Margaret.Dulat@D2L.com>
2020-12-03 16:55:11 -0800

    lint

--- components/d2l-activity-editor/d2l-activity-quiz-editor/d2l-activity-quiz-password-editor.js
+++ components/d2l-activity-editor/d2l-activity-quiz-editor/d2l-activity-quiz-password-editor.js
@@ -1,5 +1,5 @@
-import { css, html } from 'lit-element/lit-element.js';
 import { ActivityEditorMixin } from '../mixins/d2l-activity-editor-mixin.js';
+import { html } from 'lit-element/lit-element.js';
 import { LocalizeActivityQuizEditorMixin } from './mixins/d2l-activity-quiz-lang-mixin';
 import { MobxLitElement } from '@adobe/lit-mobx';
 import { RtlMixin } from '@brightspace-ui/core/mixins/rtl-mixin.js';
