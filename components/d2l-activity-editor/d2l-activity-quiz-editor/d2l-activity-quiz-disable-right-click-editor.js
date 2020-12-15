9d38dd80397d54238bdab1a0331b602367c21e61
Candice Pang <Candice.Pang@D2L.com>
2020-12-15 12:35:58 -0800

    Added container to align input-checkbox and icon-button as comment

--- components/d2l-activity-editor/d2l-activity-quiz-editor/d2l-activity-quiz-disable-right-click-editor.js
+++ components/d2l-activity-editor/d2l-activity-quiz-editor/d2l-activity-quiz-disable-right-click-editor.js
@@ -1,6 +1,6 @@
+import { css, html } from 'lit-element/lit-element.js';
 import { ActivityEditorMixin } from '../mixins/d2l-activity-editor-mixin.js';
 import { checkboxStyles } from '../styles/checkbox-styles.js';
-import { html } from 'lit-element/lit-element.js';
 import { LocalizeActivityQuizEditorMixin } from './mixins/d2l-activity-quiz-lang-mixin';
 import { MobxLitElement } from '@adobe/lit-mobx';
 import { RtlMixin } from '@brightspace-ui/core/mixins/rtl-mixin.js';
@@ -10,7 +10,20 @@ class ActivityQuizDisableRightClickEditor
 	extends ActivityEditorMixin(RtlMixin(LocalizeActivityQuizEditorMixin(MobxLitElement))) {
 
 	static get styles() {
-		return checkboxStyles;
+		return [
+			checkboxStyles,
+			css`
+				.d2l-input-checkbox-help-container {
+					display: flex;
+					align-itmes: center;
+				}
+
+				.d2l-input-checkbox-help {
+					margin: auto 0;
+					padding-right: 0.4em;
+				}
+			`
+		];
 	}
 
 	constructor() {
@@ -23,11 +36,33 @@ class ActivityQuizDisableRightClickEditor
 			return html``;
 		}
 
-		return html`
-			<d2l-input-checkbox-spacer
-				class="d2l-body-small">
-			</d2l-input-checkbox-spacer>
+		// return html`
+		// 	<d2l-input-checkbox-spacer
+		// 		class="d2l-body-small">
+		// 	</d2l-input-checkbox-spacer>
 
+		// 	<div class="d2l-input-checkbox-help-container">
+		// 		<d2l-input-checkbox
+		// 			class="d2l-input-checkbox-help"
+		// 			?checked="${entity.isDisableRightClickEnabled}"
+		// 			@change="${this._setDisableRightClick}"
+		// 			ariaLabel="${this.localize('disableRightClickDescription')}"
+		// 			?disabled="${!entity.canEditDisableRightClick}">
+		// 			${this.localize('disableRightClickDescription')}
+		// 		</d2l-input-checkbox>
+
+		// 		<d2l-button-icon
+		// 			text="${this.localize('disableRightClickAccessibleHelpText')}"
+		// 			icon="tier1:help"
+		// 			@click="${this.open}">
+		// 		</d2l-button-icon>
+		// 	</div>
+		// `;
+		return html `
+		 	<d2l-input-checkbox-spacer
+		 		class="d2l-body-small">
+			</d2l-input-checkbox-spacer>
+			 
 			<d2l-input-checkbox
 				?checked="${entity.isDisableRightClickEnabled}"
 				@change="${this._setDisableRightClick}"
