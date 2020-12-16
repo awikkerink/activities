94cb590f1220ceb6ec571bcfd98c266626548b65
Ronan Fegan <rfegan13@gmail.com>
2020-12-15 21:41:40 -0800

    Refactor checkbox container to it's own div

--- components/d2l-activity-editor/d2l-activity-quiz-editor/d2l-activity-quiz-disable-right-click-editor.js
+++ components/d2l-activity-editor/d2l-activity-quiz-editor/d2l-activity-quiz-disable-right-click-editor.js
@@ -1,6 +1,6 @@
+import { css, html } from 'lit-element/lit-element.js';
 import { ActivityEditorMixin } from '../mixins/d2l-activity-editor-mixin.js';
 import { checkboxStyles } from '../styles/checkbox-styles.js';
-import { html } from 'lit-element/lit-element.js';
 import { LocalizeActivityQuizEditorMixin } from './mixins/d2l-activity-quiz-lang-mixin';
 import { MobxLitElement } from '@adobe/lit-mobx';
 import { RtlMixin } from '@brightspace-ui/core/mixins/rtl-mixin.js';
@@ -10,7 +10,21 @@ class ActivityQuizDisableRightClickEditor
 	extends ActivityEditorMixin(RtlMixin(LocalizeActivityQuizEditorMixin(MobxLitElement))) {
 
 	static get styles() {
-		return checkboxStyles;
+		return [
+			checkboxStyles,
+			css`
+				d2l-input-checkbox {
+					margin-top: 0.8em;
+					padding-right: 0;
+				}
+
+				.d2l-checkbox-container {
+					display: flex;
+					align-items: center;
+					height: 1.2rem;
+				}
+			`
+		];
 	}
 
 	constructor() {
@@ -28,18 +42,23 @@ class ActivityQuizDisableRightClickEditor
 				class="d2l-body-small">
 			</d2l-input-checkbox-spacer>
 
-			<d2l-input-checkbox
-				?checked="${entity.isDisableRightClickEnabled}"
-				@change="${this._setDisableRightClick}"
-				ariaLabel="${this.localize('disableRightClickDescription')}"
-				?disabled="${!entity.canEditDisableRightClick}">
-				${this.localize('disableRightClickDescription')}
+			<div class="d2l-checkbox-container">
+				<d2l-input-checkbox
+					?checked="${entity.isDisableRightClickEnabled}"
+					@change="${this._setDisableRightClick}"
+					ariaLabel="${this.localize('disableRightClickDescription')}"
+					?disabled="${!entity.canEditDisableRightClick}">
+				</d2l-input-checkbox>
+
+				<label class="d2l-input-checkbox-text d2l-body-small">${this.localize('disableRightClickDescription')}</label>
+
 				<d2l-button-icon
 					text="${this.localize('disableRightClickAccessibleHelpText')}"
 					icon="tier1:help"
 					@click="${this.open}">
 				</d2l-button-icon>
-			</d2l-input-checkbox>
+			</div>
+
 		`;
 	}
 
