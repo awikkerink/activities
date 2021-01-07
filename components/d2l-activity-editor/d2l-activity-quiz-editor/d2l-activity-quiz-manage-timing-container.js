9a77b14d07e61f8ebfecba041caa5437dfcb8fbb
Mikayla Louie <Mikayla.Louie@D2L.com>
2021-01-07 09:44:24 -0800

    US123360 timing dialog UI with hardcoded strings

--- components/d2l-activity-editor/d2l-activity-quiz-editor/d2l-activity-quiz-manage-timing-container.js
+++ components/d2l-activity-editor/d2l-activity-quiz-editor/d2l-activity-quiz-manage-timing-container.js
@@ -1,3 +1,4 @@
+import './d2l-activity-quiz-manage-timing-editor.js';
 import '@brightspace-ui/core/components/dialog/dialog.js';
 import { ActivityEditorMixin } from '../mixins/d2l-activity-editor-mixin';
 import { html } from 'lit-element/lit-element';
@@ -21,7 +22,8 @@ class ActivityQuizManageTimingContainer extends ActivityEditorMixin(LocalizeActi
 
 		return html`
 		<div>
-			<d2l-dialog id="quiz-manage-timing-dialog" ?opened=${this.isDialogOpen} title-text=${this.localize('subHdrTimingTools')}>
+			<d2l-dialog id="quiz-manage-timing-dialog" ?opened=${this.isDialogOpen} width=800 title-text=${this.localize('subHdrTimingTools') }>
+			${this._renderQuizTimingEditor()}
 				<d2l-button slot="footer" primary data-dialog-action="ok">${this.localize('manageTimingDialogConfirmationText')}</d2l-button>
 				<d2l-button slot="footer" data-dialog-action>${this.localize('manageTimingDialogCancelText')}</d2l-button>
 			</d2l-dialog>
@@ -31,6 +33,15 @@ class ActivityQuizManageTimingContainer extends ActivityEditorMixin(LocalizeActi
 	// TODO: when entity is ready, check for presence `update-timing type` action. If user does not have permission, disable primary Add button
 	_canEditTiming() {
 		return true;
+	}
+
+	_renderQuizTimingEditor() {
+		return html`
+		<d2l-activity-quiz-manage-timing-editor 
+				href="${this.href}"
+				.token="${this.token}">
+		</d2l-activity-quiz-manage-timing-editor>
+		`;
 	}
 
 }
---
+++ components/d2l-activity-editor/d2l-activity-quiz-editor/d2l-activity-quiz-manage-timing-editor.js
@@ -0,0 +1,156 @@
+import './d2l-activity-quiz-manage-timing-container';
+import '@brightspace-ui/core/components/inputs/input-number.js';
+import { css, html } from 'lit-element/lit-element.js';
+import { ActivityEditorFeaturesMixin } from '../mixins/d2l-activity-editor-features-mixin.js';
+import { ActivityEditorMixin } from '../mixins/d2l-activity-editor-mixin.js';
+import { AsyncContainerMixin } from '@brightspace-ui/core/mixins/async-container/async-container-mixin.js';
+import { labelStyles } from '@brightspace-ui/core/components/typography/styles.js';
+import { LocalizeActivityQuizEditorMixin } from './mixins/d2l-activity-quiz-lang-mixin';
+import { MobxLitElement } from '@adobe/lit-mobx';
+import { radioStyles } from '@brightspace-ui/core/components/inputs/input-radio-styles.js';
+import { SkeletonMixin } from '@brightspace-ui/core/components/skeleton/skeleton-mixin.js';
+class ActivityQuizManageTimingEditor extends AsyncContainerMixin(
+	LocalizeActivityQuizEditorMixin(
+		SkeletonMixin(
+			ActivityEditorFeaturesMixin(ActivityEditorMixin(MobxLitElement))
+		)
+	)
+) {
+	static get styles() {
+		return [
+			super.styles,
+			labelStyles,
+			radioStyles,
+			css`
+				d2l-input-checkbox {
+					margin-top: 0.9rem;
+				}
+				d2l-input-number {
+					width: auto;
+				}
+				.d2l-input-radio-label {
+					padding-right: 1rem;
+				}
+				.d2l-input-radio-label:last-of-type {
+					margin-bottom: 0.9rem;
+				}
+				.d2l-input-number-label {
+					margin-left: 0.2rem;
+					margin-right: 0.2rem;
+				}
+				.d2l-time-menu-container {
+					margin: 1rem;
+				}
+				.d2l-time-enforcement-input-container {
+					display: flex;
+					flex-direction: row;
+					margin-bottom: 0.8rem;
+				}
+				.d2l-timing-option-container {
+					display: flex;
+					flex-direction: row;
+				}
+				.d2l-italic-label {
+					font-size: 0.8rem;
+					font-style: italic;
+				}
+			`,
+		];
+	}
+
+	render() {
+		const timeEnforced = true;
+		return html`
+			${this._renderTimeEnforcementOptions()}
+			${timeEnforced
+		? html`${this._renderRecommendedTimeLimitMenu()}`
+		: html`${this._renderTimeEnforcedMenu()}`}
+		`;
+	}
+
+	_renderRecommendedTimeLimitMenu() {
+		const hidden = true;
+		return html`
+			<div class="d2l-time-menu-container">
+				<div class="d2l-time-enforcement-input-container">
+					<d2l-input-number
+						label="minute(s)"
+						title="minutes(s)"
+						?label-hidden=${hidden}
+						value="120"
+					>
+						<label class="d2l-input-number-label" slot="after">minute(s)</label>
+					</d2l-input-number>
+				</div>
+				<label class="d2l-italic-label"
+					>Will be displayed before the quiz starts</label
+				>
+				<d2l-input-checkbox>Show clock</d2l-input-checkbox>
+			</div>
+		`;
+	}
+	_renderTimeEnforcedMenu() {
+		const timeEnforcmentLabels = [
+			'Allow the learner to continue working',
+			'Prevent the learner from making further changes',
+			'Allow the learner to continue working, but automatically score the attempt as zero after an extended deadline.',
+		];
+		return html`
+			<div class="d2l-time-menu-container">
+				<div class="d2l-time-enforcement-input-container">
+					<d2l-input-number label="Time Limit" title="Time Limit" value="120"
+						><label class="d2l-input-number-label" slot="after"
+							>minute(s)</label
+						></d2l-input-number
+					>
+
+					<d2l-input-number label="Grace Period" title="Time Limit" value="5"
+						><label class="d2l-input-number-label" slot="after"
+							>minute(s) before flagged as exceeded time limit</label
+						></d2l-input-number
+					>
+				</div>
+
+				<label class="d2l-label-text">Exceeded Time Limit Behaviour</label>
+				<div>
+					<label class="d2l-italic-label"
+						>After the grace period, flag the quiz attempt as exceeded time
+						limit and,</label
+					>
+				</div>
+				${timeEnforcmentLabels.map(
+		(label, i) =>
+			html`<label class="d2l-input-radio-label"
+							><input
+								type="radio"
+								name="exceededTimeBehaviour"
+								?checked=${i === 0}
+							/>${label}</label
+						>`
+	)}
+			</div>
+		`;
+	}
+	_renderTimeEnforcementOptions() {
+		const radioLabels = ['Recommended Time Limit', 'Enforced Time Limit'];
+		return html`
+			<div class="d2l-timing-option-container">
+				${radioLabels.map(
+		(label, i) =>
+			html`<label class="d2l-input-radio-label"
+							><input
+								type="radio"
+								name="timeEnforcement"
+								?checked=${i === 0}
+							/>${label}</label
+						>`
+	)}
+			</div>
+		`;
+	}
+}
+
+customElements.define(
+	'd2l-activity-quiz-manage-timing-editor',
+	ActivityQuizManageTimingEditor
+);
