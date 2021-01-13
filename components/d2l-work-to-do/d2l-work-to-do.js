506831a8b0c73b7a4ba925915e564b2eba31c9e6
Max <57962078+mbardakovD2L@users.noreply.github.com>
2021-01-13 10:05:45 -0500

    W2D: Load More button behaviour (#1323)
    feat: Load More button now loads additional pages of upcoming work

--- components/d2l-work-to-do/d2l-work-to-do.js
+++ components/d2l-work-to-do/d2l-work-to-do.js
@@ -45,7 +45,9 @@ class WorkToDoWidget extends EntityMixinLit(LocalizeWorkToDoMixin(LitElement)) {
 			/** ActivityUsageCollection with time: 0 -> UpcomingWeekLimit */
 			_upcomingCollection: { type: Object },
 			/** Represents current session's UpcomingWeekLimit */
-			_upcomingWeekLimit: { type: Number, attribute: 'data-upcoming-week-limit' }
+			_upcomingWeekLimit: { type: Number, attribute: 'data-upcoming-week-limit' },
+			/** individual items within upcomingCollection + subsequent pages once the UI has them */
+			_upcomingActivities: { type: Array }
 		};
 	}
 
@@ -123,8 +125,12 @@ class WorkToDoWidget extends EntityMixinLit(LocalizeWorkToDoMixin(LitElement)) {
 		this._upcomingCollection = undefined;
 		this._overdueWeekLimit = Config.OverdueWeekLimit;
 		this._upcomingWeekLimit = Config.UpcomingWeekLimit;
+		this._upcomingActivities = [];
+		this._overdueActivities = [];
 		this._viewAllSource = undefined;
 		this._setEntityType(UserEntity);
+
+		Rels.Activities.nextPage = 'https://activities.api.brightspace.com/rels/next-page';
 	}
 
 	set _entity(entity) {
@@ -276,19 +282,18 @@ class WorkToDoWidget extends EntityMixinLit(LocalizeWorkToDoMixin(LitElement)) {
 		const errorTemplate = nothing;
 
 		/** Fullscreen state templates */
-		const fullscreenCollectionTemplate = (collection, displayLimit, isOverdue) => {
-			if (!collection || displayLimit === 0) {
+		const fullscreenCollectionTemplate = (activities, isOverdue) => {
+			if (!activities || activities.length === 0) {
 				return nothing;
 			}
 
-			const activities = collection.getSubEntitiesByRel(Rels.Activities.userActivityUsage);
 			if (activities.length === 0) {
 				return nothing;
 			}
 
 			let prevDate = new Date(0, 0, 0, 0);
 
-			const groupedByDate = activities.slice(0, displayLimit).map((activity) => {
+			const groupedByDate = activities.map((activity) => {
 				const activityDate = activity.hasSubEntityByClass('due-date')
 					? new Date(activity.getSubEntityByClass('due-date').properties.date)
 					: new Date(activity.getSubEntityByClass('end-date').properties.date);
@@ -344,10 +349,10 @@ class WorkToDoWidget extends EntityMixinLit(LocalizeWorkToDoMixin(LitElement)) {
 				<div class="d2l-work-to-do-fullscreen-container">
 					<div class="d2l-heading-1 d2l-work-to-do-fullscreen-title">${this.localize('myWorkToDo')}</div>
 					<div class="d2l-overdue-collection-fullscreen">
-						${fullscreenCollectionTemplate(this._overdueCollection, this._overdueDisplayLimit, true)}
+						${fullscreenCollectionTemplate(this._overdueActivities, true)}
 					</div>
 					<div class="d2l-upcoming-collection-fullscreen">
-						${fullscreenCollectionTemplate(this._upcomingCollection, this._upcomingDisplayLimit, false)}
+						${fullscreenCollectionTemplate(this._upcomingActivities, false)}
 					</div>
 					${loadButtonTemplate}
 				</div>
@@ -398,9 +403,7 @@ class WorkToDoWidget extends EntityMixinLit(LocalizeWorkToDoMixin(LitElement)) {
 	}
 
 	get _moreAvail() {
-		// Logic is waiting on paging capability from activities service
-		// For now just forcing on so we can see the button
-		return true;
+		return this._upcomingCollection && this._upcomingCollection.hasLinkByRel(Rels.Activities.nextPage);
 	}
 
 	get _maxCount() {
@@ -467,10 +470,15 @@ class WorkToDoWidget extends EntityMixinLit(LocalizeWorkToDoMixin(LitElement)) {
 	 * Add to display limit so next page of activities renders for user
 	 * Load next page of activities into memory in anticipation for next request
 	 */
-	_handleLoadMoreClicked() {
-		// Nothing here right now - blocked on US120246
-		// eslint-disable-next-line no-console
-		console.log('I am an empty block of code, please complete me.');
+	async _handleLoadMoreClicked() {
+		if (!this._upcomingCollection.hasLinkByRel(Rels.Activities.nextPage)) return;
+
+		const upcomingSource = this._upcomingCollection.getLinkByRel(Rels.Activities.nextPage).href;
+		const upcomingNextPage = await fetchEntity(upcomingSource, this.token, true);
+		if (upcomingNextPage && upcomingNextPage.hasSubEntityByRel(Rels.Activities.userActivityUsage)) {
+			this._upcomingActivities = this._upcomingActivities.concat(upcomingNextPage.getSubEntitiesByRel(Rels.Activities.userActivityUsage));
+			this._upcomingCollection = upcomingNextPage; // moves "next page" forward every time this succeeds
+		}
 	}
 
 	/**
@@ -488,6 +496,7 @@ class WorkToDoWidget extends EntityMixinLit(LocalizeWorkToDoMixin(LitElement)) {
 		await fetchEntity(source, this.token)
 			.then((sirenEntity) => {
 				this._overdueCollection = sirenEntity;
+				this._overdueActivities = this._overdueCollection.getSubEntitiesByRel(Rels.Activities.userActivityUsage);
 			});
 	}
 
@@ -499,7 +508,9 @@ class WorkToDoWidget extends EntityMixinLit(LocalizeWorkToDoMixin(LitElement)) {
 	 * @param {SimpleEntity} entity - 'Empty' Activities domain endpoint response
 	 * @param {Number} [forwardLimit] - [Default: Config.UpcomingWeekLimit * 7] Number of days into future to look for activities
 	 */
-	async _loadUpcoming(entity, forwardLimit) {
+	async _loadUpcoming(entity, forwardLimit, pageSize) {
+		if (!pageSize) pageSize = Constants.MaxWidgetDisplay;
+
 		if (!entity || !entity.hasActionByName(Actions.activities.selectCustomDateRange)) {
 			return;
 		}
@@ -514,12 +525,14 @@ class WorkToDoWidget extends EntityMixinLit(LocalizeWorkToDoMixin(LitElement)) {
 		const action = entity.getActionByName(Actions.activities.selectCustomDateRange);
 		const fields = [
 			{ name: 'start', value: start },
-			{ name: 'end', value: end }
+			{ name: 'end', value: end },
+			{ name: 'pageSize', value: pageSize }
 		];
 		performSirenAction(this.token, action, fields, true)
 			.then((sirenEntity) => {
 				if (!isMax) {
 					this._upcomingCollection = sirenEntity;
+					this._upcomingActivities = sirenEntity.getSubEntitiesByRel(Rels.Activities.userActivityUsage);
 				} else {
 					this._maxCollection = sirenEntity;
 				}
