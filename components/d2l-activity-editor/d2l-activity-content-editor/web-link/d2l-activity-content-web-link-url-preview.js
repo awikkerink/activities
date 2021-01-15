4336d74754820724e95e1c634fdde1d1a89cd8fa
Dave Harris <harris.dave@gmail.com>
2021-01-15 15:44:09 -0600

    Merge pull request #1340 from BrightspaceHypermediaComponents/dharris/US123888-add-weblink-url-preview
    feat: Add support for previewing weblink urls
    (cherry picked from commit 5b15aee1567c054922f8b475a1154ceb32d85af2)

---
+++ components/d2l-activity-editor/d2l-activity-content-editor/web-link/d2l-activity-content-web-link-url-preview.js
@@ -0,0 +1,58 @@
+import '@d2l/d2l-attachment/components/attachment';
+import { css, html } from 'lit-element/lit-element.js';
+import { labelStyles } from '@brightspace-ui/core/components/typography/styles.js';
+import { LocalizeActivityEditorMixin } from '../../mixins/d2l-activity-editor-lang-mixin.js';
+import { MobxLitElement } from '@adobe/lit-mobx';
+import { RtlMixin } from '@brightspace-ui/core/mixins/rtl-mixin.js';
+import { SkeletonMixin } from '@brightspace-ui/core/components/skeleton/skeleton-mixin.js';
+
+class ContentWebLinkUrlPreview extends SkeletonMixin(LocalizeActivityEditorMixin(RtlMixin(MobxLitElement))) {
+
+	static get properties() {
+		return {
+			entity: { type: Object }
+		};
+	}
+
+	static get styles() {
+		return  [
+			super.styles,
+			labelStyles,
+			css`
+				:host {
+					display: block;
+				}
+				:host([hidden]) {
+					display: none;
+				}
+				:host > div {
+					padding-bottom: 20px;
+				}
+			`
+		];
+	}
+
+	constructor() {
+		super();
+		this.skeleton = true;
+	}
+
+	render() {
+		const attachment = {};
+		if (this.entity) {
+			this.skeleton = false;
+			attachment.id = this.entity.href;
+			attachment.name = this.entity.title;
+			attachment.url = this.entity.link;
+		}
+
+		return html`
+			<d2l-labs-attachment
+				.attachmentId="${attachment.id}"
+				.attachment="${attachment}"
+			>
+			</d2l-labs-attachment>
+		`;
+	}
+}
+customElements.define('d2l-activity-content-web-link-url-preview', ContentWebLinkUrlPreview);
