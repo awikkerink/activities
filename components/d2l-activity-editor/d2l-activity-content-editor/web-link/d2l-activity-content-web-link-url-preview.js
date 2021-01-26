68b9d29b593e3ef2fbb9ee8b3d226276495363d7
David Harris <David.Harris@D2L.com>
2021-01-26 09:40:11 -0600

    fix: preview web links skeleton

--- components/d2l-activity-editor/d2l-activity-content-editor/web-link/d2l-activity-content-web-link-url-preview.js
+++ components/d2l-activity-editor/d2l-activity-content-editor/web-link/d2l-activity-content-web-link-url-preview.js
@@ -49,13 +49,13 @@ class ContentWebLinkUrlPreview extends SkeletonMixin(LocalizeActivityEditorMixin
 		attachment.url = this.entity.link;
 
 		return html`
-	    <label class="d2l-label-text">${this.localize('content.previewLabel')}</label>
-		<d2l-labs-attachment
-			?hidden="${this.skeleton}"
-			.attachmentId="${attachment.id}"
-			.attachment="${attachment}"
-		>
-		</d2l-labs-attachment>
+			<label class="d2l-label-text">${this.localize('content.previewLabel')}</label>
+			<d2l-labs-attachment
+				?hidden="${this.skeleton}"
+				.attachmentId="${attachment.id}"
+				.attachment="${attachment}"
+			>
+			</d2l-labs-attachment>
 		`;
 	}
 }
