3c9431a655dcd53f0770c17540626ef8c0f289e3
Mikayla Louie <Mikayla.Louie@D2L.com>
2021-02-03 09:35:52 -0800

    US123360 refactor timing dialog to use mixin

--- components/d2l-activity-editor/d2l-activity-quiz-editor/d2l-activity-quiz-manage-timing-container.js
+++ components/d2l-activity-editor/d2l-activity-quiz-editor/d2l-activity-quiz-manage-timing-container.js
@@ -1,33 +1,59 @@
 import './d2l-activity-quiz-manage-timing-editor.js';
 import '@brightspace-ui/core/components/dialog/dialog.js';
+import { ActivityEditorDialogMixin } from '../mixins/d2l-activity-editor-dialog-mixin';
 import { ActivityEditorMixin } from '../mixins/d2l-activity-editor-mixin';
 import { html } from 'lit-element/lit-element';
+import { labelStyles } from '@brightspace-ui/core/components/typography/styles';
 import { LocalizeActivityQuizEditorMixin } from './mixins/d2l-activity-quiz-lang-mixin.js';
 import { MobxLitElement } from '@adobe/lit-mobx';
 import { sharedTiming as store } from './state/quiz-store';
 
-class ActivityQuizManageTimingContainer extends ActivityEditorMixin(LocalizeActivityQuizEditorMixin(MobxLitElement)) {
+class ActivityQuizManageTimingContainer extends ActivityEditorMixin(ActivityEditorDialogMixin(LocalizeActivityQuizEditorMixin(MobxLitElement))) {
+	static get styles() {
+		return [
+			labelStyles,
+		];
+	}
+
 	constructor() {
 		super(store);
 	}
 
 	render() {
 		return html`
-			<div>
-				<d2l-dialog id="quiz-manage-timing-dialog" ?opened=${this.isDialogOpen} width=800 title-text=${this.localize('subHdrTimingTools') }>
-				${this._renderQuizTimingEditor()}
+			${this._renderDialogOpener()}
+			${this._renderDialog()}
+    	`;
+	}
+
+	_renderDialog() {
+		return html`
+			<d2l-dialog
+				id="quiz-manage-timing-dialog"
+				?opened="${this.opened}"
+				@d2l-dialog-close="${this.handleClose}"
+				width=800
+				title-text=${this.localize('subHdrTimingTools') }>
+					${this._renderQuizTimingEditor()}
 					<d2l-button slot="footer" primary data-dialog-action="ok">${this.localize('manageTimingDialogConfirmationText')}</d2l-button>
 					<d2l-button slot="footer" data-dialog-action>${this.localize('manageTimingDialogCancelText')}</d2l-button>
-				</d2l-dialog>
-			</div>
-    	`;
+			</d2l-dialog>
+		`;
+	}
+
+	_renderDialogOpener() {
+		return html`
+			<div id="manage-timing-editor-label" class="d2l-label-text">${this.localize('subHdrTimingTools')}</div>
+			<div class="placeholder-for-summarizer"></div>
+			<d2l-button-subtle text=${this.localize('manageTiming')} @click="${this.open}" h-align="text"></d2l-button-subtle>
+		`;
 	}
 
 	_renderQuizTimingEditor() {
 		return html`
 			<d2l-activity-quiz-manage-timing-editor
-					href="${this.href}"
-					.token="${this.token}">
+				href="${this.href}"
+				.token="${this.token}">
 			</d2l-activity-quiz-manage-timing-editor>
 		`;
 	}
