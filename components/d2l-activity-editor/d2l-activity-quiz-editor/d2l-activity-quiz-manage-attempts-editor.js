bfcd9850a1d12a5c87b2e52e8762558c793d00cb
Mikayla Louie <58149007+me-louie@users.noreply.github.com>
2021-02-03 08:29:43 -0800

    feat: US123706 added selects, rio checkbox, button (#1462)
    * US123706 added selects, rio checkbox, button
    
    * US123706 lint

--- components/d2l-activity-editor/d2l-activity-quiz-editor/d2l-activity-quiz-manage-attempts-editor.js
+++ components/d2l-activity-editor/d2l-activity-quiz-editor/d2l-activity-quiz-manage-attempts-editor.js
@@ -1,10 +1,12 @@
 import '@brightspace-ui/core/components/dialog/dialog.js';
+import './d2l-activity-quiz-retake-incorrect-only-editor.js';
+import { css, html } from 'lit-element/lit-element';
 import { ActivityEditorDialogMixin } from '../mixins/d2l-activity-editor-dialog-mixin';
 import { ActivityEditorMixin } from '../mixins/d2l-activity-editor-mixin';
-import { html } from 'lit-element/lit-element';
 import { labelStyles } from '@brightspace-ui/core/components/typography/styles';
 import { LocalizeActivityQuizEditorMixin } from './mixins/d2l-activity-quiz-lang-mixin.js';
 import { MobxLitElement } from '@adobe/lit-mobx';
+import { selectStyles } from '@brightspace-ui/core/components/inputs/input-select-styles';
 import { shared as store } from './state/quiz-store';
 
 class ActivityQuizManageAttemptsEditor extends ActivityEditorMixin(ActivityEditorDialogMixin(LocalizeActivityQuizEditorMixin(MobxLitElement))) {
@@ -12,6 +14,12 @@ class ActivityQuizManageAttemptsEditor extends ActivityEditorMixin(ActivityEdito
 	static get styles() {
 		return [
 			labelStyles,
+			selectStyles,
+			css`
+				.d2l-input-select {
+					margin-bottom: 1.5rem;
+				}
+			`
 		];
 	}
 
@@ -25,16 +33,54 @@ class ActivityQuizManageAttemptsEditor extends ActivityEditorMixin(ActivityEdito
 		`;
 	}
 
+	_renderAttemptsCondition() {
+		// TODO: add Attempts Conditions once design has been refined
+		return html`
+			<d2l-button-subtle text=${this.localize('btnAddAttemptConditions')}></d2l-button-subtle>
+		`;
+	}
+	_renderAttemptsSelects() {
+		// TODO: replace consts with data fetched from attempts entity
+		const attemptsAllowed = ['Unlimited', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+		const gradingTypes = ['Highest Attempt', 'Lowest Attempt', 'Average of All Attempts', 'First Attempt', 'Last Attempt'];
+
+		return html`
+			<div class="d2l-label-text">${this.localize('attemptsAllowed')}</div>
+			<select class="d2l-input-select">
+				${attemptsAllowed.map((option) => html `<option value=${option}>${option}</option>`)}
+			</select>
+
+			<div class="d2l-label-text">${this.localize('overallGradeCalculation')}</div>
+			<select class="d2l-input-select">
+				${gradingTypes.map((option) => html `<option value=${option}>${option}</option>`)}
+			</select>
+		`;
+	}
+
 	_renderDialog() {
 		return html`
 			<d2l-dialog
 				id="quiz-manage-attempts-dialog"
 				?opened="${this.opened}"
 				@d2l-dialog-close="${this.handleClose}"
-				title-text="${this.localize('subHdrAttemptsTools')}">
+				title-text="${this.localize('subHdrAttemptsTools')}"
+				width=700>
+					${this._renderDialogContent()}
 					<d2l-button slot="footer" primary data-dialog-action="ok">${this.localize('manageAttemptsDialogConfirmationText')}</d2l-button>
 		 			<d2l-button slot="footer" data-dialog-action>${this.localize('manageAttemptsDialogCancelText')}</d2l-button>
 			</d2l-dialog>
+		`;
+	}
+
+	_renderDialogContent() {
+		// TODO: replace consts with data fetched from attempts entity
+		const showRio = true;
+		const showAttemptsConditions = true;
+
+		return html `
+			${this._renderAttemptsSelects()}
+			${showRio ? html`${this._renderRioOption()}` : null}
+			${showAttemptsConditions ? html `${this._renderAttemptsCondition()}` : null}
 		`;
 	}
 
@@ -47,6 +93,15 @@ class ActivityQuizManageAttemptsEditor extends ActivityEditorMixin(ActivityEdito
 				text="${this.localize('manageAttempts')}"
 				@click="${this.open}">
 			</d2l-button-subtle>
+		`;
+	}
+
+	_renderRioOption() {
+		return html `
+			<d2l-activity-quiz-retake-incorrect-only-editor
+				href="${this.href}"
+				.token="${this.token}">
+			</d2l-activity-quiz-retake-incorrect-only-editor>
 		`;
 	}
 }
