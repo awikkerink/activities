8749e91975f8e56246628114c13abcc6b4dcc932
Vincent Wong <vincent.wong@d2l.com>
2021-02-08 20:53:00 -0800

    Set to enable dialog loading spinner
    A dialog spinner appears when the dialog is open with the working copy. But it seems to disappear when the dialog href is changed and re-rendered with the forked version.
    So this change will not currently show a loading spinner.

--- components/d2l-activity-editor/d2l-activity-quiz-editor/d2l-activity-quiz-manage-timing-dialog.js
+++ components/d2l-activity-editor/d2l-activity-quiz-editor/d2l-activity-quiz-manage-timing-dialog.js
@@ -2,13 +2,14 @@ import './d2l-activity-quiz-manage-timing-editor.js';
 import '@brightspace-ui/core/components/dialog/dialog.js';
 import { ActivityEditorDialogMixin } from '../mixins/d2l-activity-editor-dialog-mixin';
 import { ActivityEditorMixin } from '../mixins/d2l-activity-editor-mixin';
+import { AsyncContainerMixin } from '@brightspace-ui/core/mixins/async-container/async-container-mixin.js';
 import { css, html } from 'lit-element/lit-element';
 import { LocalizeActivityQuizEditorMixin } from './mixins/d2l-activity-quiz-lang-mixin.js';
 import { MobxLitElement } from '@adobe/lit-mobx';
 import { sharedTiming as store } from './state/quiz-store';
 
 
-class ActivityQuizManageTimingDialog extends ActivityEditorMixin(ActivityEditorDialogMixin(LocalizeActivityQuizEditorMixin(MobxLitElement))) {
+class ActivityQuizManageTimingDialog extends AsyncContainerMixin(ActivityEditorMixin(ActivityEditorDialogMixin(LocalizeActivityQuizEditorMixin(MobxLitElement)))) {
 	static get styles() {
 		return [
 			css`
@@ -30,10 +31,12 @@ class ActivityQuizManageTimingDialog extends ActivityEditorMixin(ActivityEditorD
 	}
 
 	_renderDialog() {
+		const dialogAsync = true;
 		return html`
 			<d2l-dialog
 				id="quiz-manage-timing-dialog"
 				?opened="${this.opened}"
+				?async="${dialogAsync}"
 				@d2l-dialog-close="${this.handleClose}"
 				width=800
 				title-text=${this.localize('subHdrTimingTools') }>
