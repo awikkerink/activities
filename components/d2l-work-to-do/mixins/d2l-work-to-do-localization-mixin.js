11fee9757b2269b89407d0f0268cf767fe648bfe
Danny Gleckler <daniel.gleckler@d2l.com>
2021-02-25 15:01:43 -0500

    fix: Move w2d to variable dynamic locale imports (#1540)
    * Move w2d to dynamic locale imports
    
    * Cleanup
    
    * Bump eslint-config-brightspace
    
    * Add explicit supportedLangs
    
    * Remove supportedLangs
    
    * Update for final mixin syntax
    
    * Bump core

--- components/d2l-work-to-do/mixins/d2l-work-to-do-localization-mixin.js
+++ components/d2l-work-to-do/mixins/d2l-work-to-do-localization-mixin.js
@@ -1,86 +1,11 @@
-import { LocalizeMixin } from '@brightspace-ui/core/mixins/localize-mixin.js';
-import { getLocalizeOverrideResources } from '@brightspace-ui/core/helpers/getLocalizeResources.js';
+import { LocalizeDynamicMixin } from '@brightspace-ui/core/mixins/localize-dynamic-mixin.js';
 
-export const LocalizeWorkToDoMixin = superclass => class extends LocalizeMixin(superclass) {
+export const LocalizeWorkToDoMixin = superclass => class extends LocalizeDynamicMixin(superclass) {
 
-	static async getLocalizeResources(langs) {
-
-		function resolveOverridesFunc() {
-			return 'd2l-activities\\workToDo';
-		}
-
-		let translations;
-		for await (const lang of langs) {
-			switch (lang) {
-				case 'ar':
-					translations = await import('../lang/ar.js');
-					break;
-				case 'cy-gb':
-					translations = await import('../lang/cy-gb.js');
-					break;
-				case 'cy':
-					translations = await import('../lang/cy.js');
-					break;
-				case 'da':
-					translations = await import('../lang/da.js');
-					break;
-				case 'de':
-					translations = await import('../lang/de.js');
-					break;
-				case 'en':
-					translations = await import('../lang/en.js');
-					break;
-				case 'es-es':
-					translations = await import('../lang/es-es.js');
-					break;
-				case 'es':
-					translations = await import('../lang/es.js');
-					break;
-				case 'fr-ca':
-					translations = await import('../lang/fr-ca.js');
-					break;
-				case 'fr':
-					translations = await import('../lang/fr.js');
-					break;
-				case 'ja':
-					translations = await import('../lang/ja.js');
-					break;
-				case 'ko':
-					translations = await import('../lang/ko.js');
-					break;
-				case 'nl':
-					translations = await import('../lang/nl.js');
-					break;
-				case 'pt':
-					translations = await import('../lang/pt.js');
-					break;
-				case 'sv':
-					translations = await import('../lang/sv.js');
-					break;
-				case 'tr':
-					translations = await import('../lang/tr.js');
-					break;
-				case 'zh-tw':
-					translations = await import('../lang/zh-tw.js');
-					break;
-				case 'zh':
-					translations = await import('../lang/zh.js');
-					break;
-			}
-			if (translations && translations.val) {
-				return await getLocalizeOverrideResources(
-					lang,
-					translations.val,
-					resolveOverridesFunc
-				);
-			}
-		}
-
-		translations = await import('../lang/en.js');
-		return await getLocalizeOverrideResources(
-			'en',
-			translations.val,
-			resolveOverridesFunc
-		);
+	static get localizeConfig() {
+		return {
+			importFunc: async lang => (await import(`../lang/${lang}.js`)).val,
+			osloCollection: 'd2l-activities\\workToDo',
+		};
 	}
 };
