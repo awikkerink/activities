4e79032583a369bc95644d8ef12458f1dea5d8cd
Scy Akulick <50635849+Scy-D2L@users.noreply.github.com>
2021-03-02 10:43:17 -0500

    fix: Add/Upate lang files for d2l-activity-date (#1577)

---
+++ components/d2l-activity-date/lang/da-dk.js
@@ -0,0 +1,14 @@
+export const val = {
+	onlyDate : "{date}", // Displays the date of the provided activity's date sub-entity -  with date string formatted to match request
+	onlyDateTimed : "{date} kl. {time}", // Displays the date and time of the provided activity's date sub-entity - with date string formatted to match request
+	dueDate : "Forfalder d. {date}", // Displays the due-date of the provided activity - with date string formatted to match request
+	dueDateTimed : "Forfalder d. {date} kl. {time}", // Displays the due-date, with time, of the provided activity - with date string formatted to match request
+	endDate : "Slutter d. {date}", // Displays the end-date of the provided activity - with date string formatted to match request
+	endDateTimed : "Slutter d. {date} kl. {time}", // Displays the end-date, with time, of the provided activity - with date string formatted to match request
+	issueDate : "Udstedt d. {date}", // Displays the issue-date of the provided activity - with date string formatted to match request
+	issueDateTimed : "Udstedt d. {date} kl. {time}", // Displays the issue-date, with time, of the provided activity - with date string formatted to match request
+	startDate : "Starter d. {date}", // Displays date of a future start-date of the provided activity - with date string formatted to match request
+	startDateTimed : "Starter d. {date} kl. {time}", // Displays date and time of a future start-date of the provided activity - with date string formatted to match request
+	startedDate : "Startede d. {date}", // Displays date of a past start-date of the provided activity - with date string formatted to match request
+	startedDateTimed : "Startede d. {date} kl. {time}", // Displays date and time of a past start-date of the provided activity - with date string formatted to match request
+};
--- components/d2l-activity-date/lang/ko.js
+++ components/d2l-activity-date/lang/ko.js
@@ -1,14 +1,14 @@
 export const val = {
 	onlyDate : "{date}", // Displays the date of the provided activity's date sub-entity -  with date string formatted to match request
-	onlyDateTimed : "{date} at {time}", // Displays the date and time of the provided activity's date sub-entity - with date string formatted to match request
-	dueDate : "Due {date}", // Displays the due-date of the provided activity - with date string formatted to match request
-	dueDateTimed : "Due {date} at {time}", // Displays the due-date, with time, of the provided activity - with date string formatted to match request
-	endDate : "Ends {date}", // Displays the end-date of the provided activity - with date string formatted to match request
-	endDateTimed : "Ends {date} at {time}", // Displays the end-date, with time, of the provided activity - with date string formatted to match request
-	issueDate : "Issued {date}", // Displays the issue-date of the provided activity - with date string formatted to match request
-	issueDateTimed : "Issued {date} at {time}", // Displays the issue-date, with time, of the provided activity - with date string formatted to match request
-	startDate : "Starts {date}", // Displays date of a future start-date of the provided activity - with date string formatted to match request
-	startDateTimed : "Starts {date} at {time}", // Displays date and time of a future start-date of the provided activity - with date string formatted to match request
-	startedDate : "Started {date}", // Displays date of a past start-date of the provided activity - with date string formatted to match request
-	startedDateTimed : "Started {date} at {time}", // Displays date and time of a past start-date of the provided activity - with date string formatted to match request
+	onlyDateTimed : "{date}일 {time}시", // Displays the date and time of the provided activity's date sub-entity - with date string formatted to match request
+	dueDate : "기한 {date}", // Displays the due-date of the provided activity - with date string formatted to match request
+	dueDateTimed : "{date}일 {time}시에 마감", // Displays the due-date, with time, of the provided activity - with date string formatted to match request
+	endDate : "종료일 {date}", // Displays the end-date of the provided activity - with date string formatted to match request
+	endDateTimed : "{date}일 {time}에 종료", // Displays the end-date, with time, of the provided activity - with date string formatted to match request
+	issueDate : "{date}에 발행됨", // Displays the issue-date of the provided activity - with date string formatted to match request
+	issueDateTimed : "{date}일 {time}시에 발행됨", // Displays the issue-date, with time, of the provided activity - with date string formatted to match request
+	startDate : "시작일 {date}", // Displays date of a future start-date of the provided activity - with date string formatted to match request
+	startDateTimed : "{date}일 {time}에 시작", // Displays date and time of a future start-date of the provided activity - with date string formatted to match request
+	startedDate : "{date}에 시작됨", // Displays date of a past start-date of the provided activity - with date string formatted to match request
+	startedDateTimed : "{date}일 {time}에 시작됨", // Displays date and time of a past start-date of the provided activity - with date string formatted to match request
 };
