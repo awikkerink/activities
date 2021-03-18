532ec067a47bad7161440ba4171ce94a6ff55965
Danny Gleckler <daniel.gleckler@d2l.com>
2021-03-18 19:56:50 -0400

    fix: Temporarily copy quick-eval-widget es.js to es-es.js (#1617)

---
+++ components/d2l-quick-eval-widget/lang/es-es.js
@@ -0,0 +1,8 @@
+/* eslint quotes: 0 */
+
+export default {
+	"CaughtUp": "Ya está al día.",
+	"Error": "¡Ups! Se produjo un error y no se pudieron cargar los elementos. Actualice esta página o vuelva a intentarlo más tarde.",
+	"NoSubmissions": "No tiene envíos que necesiten evaluación. Vuelva a revisar más tarde para ver los envíos nuevos.",
+	"ViewAllActivities": "Ver todas las actividades",
+};
