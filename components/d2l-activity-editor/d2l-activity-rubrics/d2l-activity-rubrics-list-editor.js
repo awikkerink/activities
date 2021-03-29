6457d662b132beae287a8a407b0ce098d24b4a9d
Uzair Shamim <Uzair.Shamim@D2L.com>
2021-03-29 17:29:26 -0400

    Remove unused property

--- components/d2l-activity-editor/d2l-activity-rubrics/d2l-activity-rubrics-list-editor.js
+++ components/d2l-activity-editor/d2l-activity-rubrics/d2l-activity-rubrics-list-editor.js
@@ -96,7 +96,6 @@ class ActivityRubricsListEditor extends ActivityEditorMixin(LocalizeActivityEdit
 				<d2l-rubric
 					class="d2l-association-box"
 					force-compact
-					outcomes-tool-integration-enabled
 					.href="${association.rubricHref}"
 					.token="${this.token}">
 				</d2l-rubric>
