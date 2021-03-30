bc10b8b8b1e1e38c9165c26019877df344c837c7
Uzair Shamim <Uzair.Shamim@D2L.com>
2021-03-30 15:52:33 -0400

    Added tests for Rubrics List Editor

--- test/d2l-activity-editor/d2l-activity-rubrics/d2l-activity-rubrics-list-container.test.js
+++ test/d2l-activity-editor/d2l-activity-rubrics/d2l-activity-rubrics-list-container.test.js
@@ -1,4 +1,4 @@
-import '../../../components/d2l-activity-editor/d2l-activity-rubrics/d2l-activity-rubrics-list-container.js';
+import '../../../components/d2l-activity-editor/d2l-activity-rubrics/d2l-activity-rubrics-list-wrapper.js';
 import { expect, fixture, html, oneEvent } from '@open-wc/testing';
 import { ActivityUsage } from '../../../components/d2l-activity-editor/state/activity-usage.js';
 import { shared as activityUsageStore } from '../../../components/d2l-activity-editor/state/activity-store.js';
@@ -6,11 +6,13 @@ import { Assignment } from '../../../components/d2l-activity-editor/d2l-activity
 import { shared as assignmentStore } from '../../../components/d2l-activity-editor/d2l-activity-assignment-editor/state/assignment-store.js';
 import { AssociationCollection } from '../../../components/d2l-activity-editor/d2l-activity-rubrics/state/association-collection.js';
 import { default as associationStore } from '../../../components/d2l-activity-editor/d2l-activity-rubrics/state/association-collection-store.js';
+import { default as langTerms } from '../../../components/d2l-activity-editor/lang/en.js';
 import { runConstructor } from '@brightspace-ui/core/tools/constructor-test-helper.js';
+import sinon from 'sinon';
 
-describe('d2l-activity-assignment-editor-evaluation-editor', function() {
+describe('d2l-activity-rubrics-list-wrapper', function() {
 
-	let href, token, associationCollection, assignment, activity, milestones;
+	let href, token, associationCollection, assignment, activity;
 
 	function _requestProviderMock(e) {
 		e.detail.provider = 'hello';
@@ -24,12 +26,13 @@ describe('d2l-activity-assignment-editor-evaluation-editor', function() {
 
 		return await fixture(
 			html`
-				<d2l-activity-rubrics-list-container
+				<d2l-activity-rubrics-list-wrapper
 					href=${href}
 					activityUsageHref=${href}
 					.token="${token}"
+					assignmentHref=${href}
 				>
-				</d2l-activity-rubrics-list-container>
+				</d2l-activity-rubrics-list-wrapper>
 			`
 		);
 	}
@@ -48,7 +51,28 @@ describe('d2l-activity-assignment-editor-evaluation-editor', function() {
 
 		associationCollection = new AssociationCollection(href, token);
 		associationCollection.defaultScoringRubricOptions = [];
-		associationCollection.addPotentialAssociationToMap(href, {});
+
+		sinon.stub(associationCollection, 'fetchAssociations').callsFake(() =>
+			[
+				{
+					isAssociated: true,
+					isAssociating: false,
+					isDeleting: false,
+					entity: {
+						canDeleteAssociation: sinon.stub().returns(true)
+					}
+				},
+				{
+					isAssociated: true,
+					isAssociating: false,
+					isDeleting: false,
+					entity: {
+						canDeleteAssociation: sinon.stub().returns(false)
+					}
+				}
+			]
+		);
+		// associationCollection.addPotentialAssociationToMap(href, );
 
 		assignment = new Assignment(href, token);
 		assignment.canEditAnnotations = true;
@@ -56,6 +80,7 @@ describe('d2l-activity-assignment-editor-evaluation-editor', function() {
 
 		activity = new ActivityUsage(href, token);
 		activity.canEditCompetencies = true;
+		activity.associationsHref = href;
 	});
 
 	afterEach(() => {
@@ -67,7 +92,7 @@ describe('d2l-activity-assignment-editor-evaluation-editor', function() {
 
 		it('should construct', async() => {
 			await loadComponent();
-			runConstructor('d2l-activity-rubrics-list-container');
+			runConstructor('d2l-activity-rubrics-list-wrapper');
 		});
 
 	});
@@ -77,9 +102,46 @@ describe('d2l-activity-assignment-editor-evaluation-editor', function() {
 		await expect(el).to.be.accessible();
 	});
 
-	it('has rubrics list editor', async() => {
+	it('has rubrics heading', async() => {
 		const el = await loadComponent();
-		const editor = el.shadowRoot.querySelector('d2l-activity-rubrics-list-editor');
-		expect(editor).to.not.be.null;
+		const containerComponent = el.shadowRoot.querySelector('d2l-activity-rubrics-list-container');
+		const container = await fixture(html`${containerComponent}`);
+
+		const containerHeadingComponent = container.shadowRoot.querySelector('.d2l-rubric-heading-container');
+
+		const rubricsHeading = containerHeadingComponent.querySelectorAll('.d2l-rubric-heading-title')[0];
+		expect(rubricsHeading.innerText).to.equal(langTerms['rubrics.hdrRubrics']);
+	});
+
+	describe('Rubrics List Editor', () => {
+		let el, containerComponent, container, rubricsListEditor;
+		beforeEach(async() => {
+			el = await loadComponent();
+			containerComponent = el.shadowRoot.querySelector('d2l-activity-rubrics-list-container');
+			container = await fixture(html`${containerComponent}`);
+			rubricsListEditor = container.shadowRoot.querySelector('d2l-activity-rubrics-list-editor');
+		});
+
+		it('has rubrics list editor', async() => {
+			const rubricsListEditorComponent = await fixture(html`${rubricsListEditor}`);
+
+			const rubricComponent = rubricsListEditorComponent.shadowRoot.querySelectorAll('.d2l-association-container > d2l-rubric')[0];
+			expect(rubricComponent).to.not.be.null;
+
+			const rubricIconComponent = rubricsListEditorComponent.shadowRoot.querySelectorAll('.d2l-association-container > d2l-button-icon')[0];
+			expect(rubricIconComponent.text).to.equal(langTerms['rubrics.txtDeleteRubric']);
+			expect(rubricIconComponent.hidden).to.equal(false);
+		});
+
+		it('hides delete association button when canDeleteAssociation is false', async() => {
+			const rubricsListEditorComponent = await fixture(html`${rubricsListEditor}`);
+
+			const rubricComponent = rubricsListEditorComponent.shadowRoot.querySelectorAll('.d2l-association-container > d2l-rubric')[1];
+			expect(rubricComponent).to.not.be.null;
+
+			const rubricIconComponent = rubricsListEditorComponent.shadowRoot.querySelectorAll('.d2l-association-container > d2l-button-icon')[1];
+			expect(rubricIconComponent.text).to.equal(langTerms['rubrics.txtDeleteRubric']);
+			expect(rubricIconComponent.hidden).to.equal(true);
+		});
 	});
 });
