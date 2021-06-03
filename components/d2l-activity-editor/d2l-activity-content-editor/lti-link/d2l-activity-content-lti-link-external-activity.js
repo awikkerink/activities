373ed6fead790074152c0cc3b93853851f2dc027
Diljot Garcha <Diljot.Garcha@D2L.com>
2021-06-03 09:39:52 -0500

    Update PR based off PR comments

--- components/d2l-activity-editor/d2l-activity-content-editor/lti-link/d2l-activity-content-lti-link-external-activity.js
+++ components/d2l-activity-editor/d2l-activity-content-editor/lti-link/d2l-activity-content-lti-link-external-activity.js
@@ -1,9 +1,9 @@
 import '@brightspace-ui/core/components/button/button-subtle.js';
-import { html, css } from 'lit-element/lit-element.js';
-import { MobxLitElement } from '@adobe/lit-mobx';
-import { LocalizeActivityEditorMixin } from '../../mixins/d2l-activity-editor-lang-mixin.js';
-import { RtlMixin } from '@brightspace-ui/core/mixins/rtl-mixin.js';
+import { css, html } from 'lit-element/lit-element.js';
 import { labelStyles } from '@brightspace-ui/core/components/typography/styles.js';
+import { LocalizeActivityEditorMixin } from '../../mixins/d2l-activity-editor-lang-mixin.js';
+import { MobxLitElement } from '@adobe/lit-mobx';
+import { RtlMixin } from '@brightspace-ui/core/mixins/rtl-mixin.js';
 
 class ExternalActivity extends LocalizeActivityEditorMixin(RtlMixin(MobxLitElement)) {
 
@@ -15,7 +15,7 @@ class ExternalActivity extends LocalizeActivityEditorMixin(RtlMixin(MobxLitEleme
 
 	static get styles() {
 		return [
-			css`	
+			css`
 			.d2l-content-link-external-activity {
 				display: flex;
 				justify-content: space-between;
@@ -26,20 +26,16 @@ class ExternalActivity extends LocalizeActivityEditorMixin(RtlMixin(MobxLitEleme
 		];
 	}
 
-	constructor() {
-		super();
-	}
-
 	render() {
 		return html`
 		<div class="d2l-content-link-external-activity">
-			<div class="d2l-heading-2">
+			<div class="d2l-label-text">
 				${this.localize('content.externalActivity')}
 			</div>
             <d2l-button-subtle
 				text="${this.localize('content.openInNewWindow')}"
 				icon="tier1:new-window"
-				@click="${this._onClick}">
+				@click="${this._onClick}"
 			>
             </d2l-button-subtle>
 		</div>
@@ -47,7 +43,7 @@ class ExternalActivity extends LocalizeActivityEditorMixin(RtlMixin(MobxLitEleme
 	}
 
 	_onClick() {
-		window.open(this.entity.link, '_blank'); // TODO: Doesn't replace {orgUnitId} with the actual orgUnitId
+		window.open(this.entity.link, '_blank');
 	}
 }
 customElements.define('d2l-activity-content-lti-link-external-activity', ExternalActivity);
