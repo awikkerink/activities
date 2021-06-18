4609bb0cdb11775f0333f2445ef386e417aa296d
Margaret Dulat <Margaret.Dulat@D2L.com>
2021-06-17 17:24:40 -0700

    fix: don't show category button if no categories, don't show type/scheme button if no schemes

--- components/d2l-activity-editor/d2l-activity-grades/d2l-activity-edit-new-grade.js
+++ components/d2l-activity-editor/d2l-activity-grades/d2l-activity-edit-new-grade.js
@@ -1,11 +1,12 @@
 import './d2l-activity-grade-category-selector.js';
 import './d2l-activity-grade-type-scheme-selector.js';
+import { sharedAssociateGrade as associateGradeStore, shared as store } from '../state/activity-store.js';
 import { css, html } from 'lit-element/lit-element';
 import { ActivityEditorMixin } from '../mixins/d2l-activity-editor-mixin.js';
+import { GradeType } from 'siren-sdk/src/activities/associateGrade/AssociateGradeEntity.js';
 import { LocalizeActivityEditorMixin } from '../mixins/d2l-activity-editor-lang-mixin.js';
 import { MobxLitElement } from '@adobe/lit-mobx';
 import { RtlMixin } from '@brightspace-ui/core/mixins/rtl-mixin.js';
-import { shared as store } from '../state/activity-store.js';
 
 class ActivityEditNewGrade extends ActivityEditorMixin(LocalizeActivityEditorMixin(RtlMixin(MobxLitElement))) {
 	static get properties() {
@@ -46,26 +47,35 @@ class ActivityEditNewGrade extends ActivityEditorMixin(LocalizeActivityEditorMix
 	}
 
 	render() {
+		const associateGradeEntity = associateGradeStore.get(this._associateGradeHref);
+		if (!associateGradeEntity || !associateGradeEntity.gradeCategoryCollection || !associateGradeEntity.gradeSchemeCollection) {
+			return html``;
+		}
+		const categoriesEmpty =	associateGradeEntity.gradeCategoryCollection.gradeCategories.length === 0;
+		const schemesEmpty = associateGradeEntity.gradeType === GradeType.Selectbox ?
+			associateGradeEntity.gradeSchemeCollection.gradeSchemes.length === 0 :
+			associateGradeEntity.gradeSchemeCollection.gradeSchemes.length === 1;
+
 		return html`
 		<div class="d2l-activity-grades-dialog-property-buttons">
 			<d2l-button-subtle
-				?hidden="${this._showCategories}"
+				?hidden="${this._showCategories || categoriesEmpty}"
 				text="${this.localize('grades.chooseNewGradeItemCategory')}"
 				@click="${this.showCategories}">
 			</d2l-button-subtle>
 			<d2l-button-subtle
-				?hidden="${this._showTypes}"
+				?hidden="${this._showTypes || schemesEmpty}"
 				text="${this.localize('grades.changeNewGradeTypeAndScheme')}"
 				@click="${this.showTypes}">
 			</d2l-button-subtle>
 		</div>
 		<d2l-activity-grade-category-selector
-			?hidden="${!this._showCategories}"
+			?hidden="${!this._showCategories || categoriesEmpty}"
 			.href="${this.href}"
 			.token="${this.token}">
 		</d2l-activity-grade-category-selector>
 		<d2l-activity-grade-type-scheme-selector
-			?hidden="${!this._showTypes}"
+			?hidden="${!this._showTypes || schemesEmpty}"
 			.href="${this._associateGradeHref}"
 			.token="${this.token}">
 		</d2l-activity-grade-type-scheme-selector>
