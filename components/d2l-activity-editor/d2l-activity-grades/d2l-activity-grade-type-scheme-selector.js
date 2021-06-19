5ebe2d530c3c720ee602835cdf9793117b66ba1c
Vincent Wong <vincent.wong@d2l.com>
2021-06-18 17:00:23 -0700

    Don't refetch schemes entity on select
    Use linked scheme entity in associate-grade entity

--- components/d2l-activity-editor/d2l-activity-grades/d2l-activity-grade-type-scheme-selector.js
+++ components/d2l-activity-editor/d2l-activity-grades/d2l-activity-grade-type-scheme-selector.js
@@ -56,7 +56,8 @@ class ActivityGradeTypeSchemeSelector extends ActivityEditorMixin(LocalizeActivi
 		const {
 			canEditNewGrade,
 			gradeType,
-			gradeSchemeCollection
+			gradeSchemeCollection,
+			selectedSchemeHref
 		} = associateGrade;
 
 		return html`
@@ -100,7 +101,7 @@ class ActivityGradeTypeSchemeSelector extends ActivityEditorMixin(LocalizeActivi
 					@change="${this._setSelectedScheme}"
 					>
 					${gradeSchemeCollection && gradeSchemeCollection.gradeSchemes.map(scheme => html`
-						<option value="${scheme.href}" .selected="${scheme.isSelected}">
+						<option value="${scheme.href}" .selected="${scheme.href == selectedSchemeHref}">
 							${scheme.isDefault ?
 		this.localize('grades.defaultGradeScheme', { schemeName: scheme.name })
 		: scheme.name
