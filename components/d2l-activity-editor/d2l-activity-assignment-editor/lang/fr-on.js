1936bc7db07bc5760c64ac5642100e136b9b0ed1
Candice Pang <Candice.Pang@D2L.com>
2021-07-14 23:03:26 -0700

    First commit
    Signed-off-by: github-actions <github-actions@github.com>

--- components/d2l-activity-editor/d2l-activity-assignment-editor/lang/fr-on.js
+++ components/d2l-activity-editor/d2l-activity-assignment-editor/lang/fr-on.js
@@ -1,7 +1,7 @@
 /* eslint quotes: 0 */
 
 export default {
-	"hdrReleaseConditions": "Conditions de diffusion", // release conditions heading
+	"hdrReleaseConditions": "Some fr-on lang term change", // release conditions heading
 	"hlpReleaseConditions": "Les utilisateurs ne peuvent pas accéder à l’affectation ou la visualiser s’ils ne remplissent pas les conditions de validation.", // release conditions help
 	"completionType": "Identifié comme étant Terminé", // Label for the completion type field when creating/editing an assignment
 	"lblAnonymousMarking": "Attribution anonyme d’état", // Label for anonymous marking
