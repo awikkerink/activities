dcaf76fe5bef238fc4291f1ce95f0e2be803dd75
Martin Wang <Martin.Wang@D2L.com>
2021-08-31 15:50:44 -0700

    UI fixes

--- components/d2l-activity-editor/d2l-activity-quiz-editor/d2l-activity-quiz-auto-set-graded-editor.js
+++ components/d2l-activity-editor/d2l-activity-quiz-editor/d2l-activity-quiz-auto-set-graded-editor.js
@@ -1,13 +1,20 @@
+import '@brightspace-ui/core/components/button/button-icon.js';
+import '@brightspace-ui/core/components/dialog/dialog.js';
+import { ActivityEditorDialogMixin } from '../mixins/d2l-activity-editor-dialog-mixin.js';
 import { ActivityEditorMixin } from '../mixins/d2l-activity-editor-mixin.js';
 import { checkboxStyles } from '../styles/checkbox-styles.js';
-import { html } from 'lit-element/lit-element.js';
+import { css, html } from 'lit-element/lit-element.js';
 import { LocalizeActivityQuizEditorMixin } from './mixins/d2l-activity-quiz-lang-mixin';
 import { MobxLitElement } from '@adobe/lit-mobx';
 import { RtlMixin } from '@brightspace-ui/core/mixins/rtl-mixin.js';
 import { shared as store } from './state/quiz-store';
 
+import { AsyncContainerMixin } from '@brightspace-ui/core/mixins/async-container/async-container-mixin.js';
+import { labelStyles } from '@brightspace-ui/core/components/typography/styles.js';
+import { SkeletonMixin } from '@brightspace-ui/core/components/skeleton/skeleton-mixin.js';
+
 class ActivityQuizAutomaticGradeEditor
-	extends ActivityEditorMixin(RtlMixin(LocalizeActivityQuizEditorMixin(MobxLitElement))) {
+	extends ActivityEditorMixin(RtlMixin(LocalizeActivityQuizEditorMixin(ActivityEditorDialogMixin(MobxLitElement)))) {
 
 	static get styles() {
 		return checkboxStyles;
@@ -27,14 +34,43 @@ class ActivityQuizAutomaticGradeEditor
 			<d2l-input-checkbox-spacer
 				class="d2l-body-small">
 			</d2l-input-checkbox-spacer>
+		
+				<d2l-input-checkbox 
+					?checked="${entity.isAutoSetGradedEnabled}"
+					@change="${this._setAutoSetGraded}"
+					ariaLabel="${this.localize('autoSetGradedDescription')}"
+					?disabled="${!entity.canEditAutoSetGraded}">
 
-			<d2l-input-checkbox
-				?checked="${entity.isAutoSetGradedEnabled}"
-				@change="${this._setAutoSetGraded}"
-				ariaLabel="${this.localize('autoSetGradedDescription')}"
-				?disabled="${!entity.canEditAutoSetGraded}">
-				${this.localize('autoSetGradedDescription')}
-			</d2l-input-checkbox>
+					<label> ${this.localize('autoSetGradedDescription')} </label>
+
+					<d2l-button-icon
+						text="${this.localize('autoSetGradedAccessibleHelpText')}"
+						icon="tier1:help"
+						@click="${this.open}">
+					</d2l-button-icon>
+					
+				</d2l-input-checkbox>
+				${this._renderDialog()}
+		`;
+	}
+
+	_renderDialog() {
+		return html`
+			<d2l-dialog
+				?opened="${this.opened}"
+				@d2l-dialog-close="${this.handleClose}"
+				title-text="${this.localize('autoSetGradedHelpDialogTitle')}">
+					<div>
+						<p>${this.localize('autoSetGradedHelpDialogParagraph1')}</p>
+						<p>${this.localize('autoSetGradedHelpDialogParagraph2')}</p>
+					</div>
+					<d2l-button
+						data-dialog-action="done"
+						slot="footer"
+						primary>
+						${this.localize('autoSetGradedHelpDialogConfirmationText')}
+					</d2l-button>
+			</d2l-dialog>
 		`;
 	}
 
