16e69c1ecd483134e00ca1effa01fa44a9fbbb68
Devon Powell <Devon.Powell@D2L.com>
2021-09-17 15:27:12 -0400

    fix: change to new top level component (#1980)

--- components/d2l-activity-editor/d2l-activity-rubrics/d2l-activity-rubrics-list-container.js
+++ components/d2l-activity-editor/d2l-activity-rubrics/d2l-activity-rubrics-list-container.js
@@ -2,7 +2,7 @@ import './d2l-activity-rubrics-list-editor';
 import 'd2l-associations/add-associations.js';
 import 'd2l-rubric/d2l-rubric';
 import 'd2l-rubric/d2l-rubric-title';
-import 'd2l-rubric/editor/d2l-rubric-editor.js';
+import 'd2l-rubric/d2l-rubric-editor.js';
 import '@brightspace-ui/core/components/dialog/dialog-fullscreen.js';
 import '@brightspace-ui/core/components/dropdown/dropdown.js';
 import '@brightspace-ui/core/components/dropdown/dropdown-content.js';
