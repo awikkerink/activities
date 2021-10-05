ae43cf581462b08d8c11891019eb40c0e3d3343a
Vincent Wong <vincent.wong@d2l.com>
2021-10-05 12:45:02 -0700

    Clean up some submission views accordion and dialog stuff

---
+++ components/d2l-activity-editor/d2l-activity-quiz-editor/d2l-activity-quiz-submission-views-accordion-editor.js
@@ -0,0 +1,63 @@
+import { css, html } from 'lit-element/lit-element.js';
+import { ActivityEditorMixin } from '../mixins/d2l-activity-editor-mixin.js';
+import { checkboxStyles } from '../styles/checkbox-styles.js';
+import { LocalizeActivityQuizEditorMixin } from './mixins/d2l-activity-quiz-lang-mixin';
+import { labelStyles } from '@brightspace-ui/core/components/typography/styles.js';
+import { MobxLitElement } from '@adobe/lit-mobx';
+import { RtlMixin } from '@brightspace-ui/core/mixins/rtl-mixin.js';
+import { selectStyles } from '@brightspace-ui/core/components/inputs/input-select-styles.js';
+import { sharedSubmissionViews as store } from './state/quiz-store';
+
+class ActivityQuizSubmissionViewsAccordionEditor
+	extends ActivityEditorMixin(RtlMixin(LocalizeActivityQuizEditorMixin(MobxLitElement))) {
+
+	static get styles() {
+		return [
+			labelStyles,
+			selectStyles,
+			checkboxStyles,
+			css`
+				.d2l-label-text {
+					padding-bottom: 10px;
+				}
+			`
+		];
+	}
+
+	constructor() {
+		super(store);
+	}
+
+	render() {
+		return html`
+			<div class="d2l-label-text">
+				${this.localize('submissionViewHeading1')}
+			</div>
+			<d2l-input-checkbox
+				?checked=""
+				@change=""
+				ariaLabel="${this.localize('submissionViewCheckboxLabel')}"
+				?disabled="">
+				${this.localize('submissionViewCheckboxLabel')}
+			</d2l-input-checkbox>
+
+			<div class="d2l-label-text">
+				${this.localize('submissionViewHeading2')}
+			</div>
+			<div>
+				<select
+					id="submission-view-editor"
+					class="d2l-input-select d2l-block-select"
+					@change="">
+					<option value="">API Placeholder 1</option>
+					<option value="">API Placeholder 2</option>
+				</select>
+			</div>
+		`;
+	}
+}
+
+customElements.define(
+	'd2l-activity-quiz-submission-views-accordion-editor',
+	ActivityQuizSubmissionViewsAccordionEditor
+);
--- components/d2l-activity-editor/d2l-activity-quiz-editor/d2l-activity-quiz-submission-views-editor.js
+++ components/d2l-activity-editor/d2l-activity-quiz-editor/d2l-activity-quiz-submission-views-editor.js
@@ -20,21 +20,27 @@ class ActivityQuizSubmissionViewsEditor
 		}
 
 		return html`
-			<label>${this.localize('submissionViewHeading1')}</label>
-			<d2l-button-icon
-				icon="tier1:help"
-				@click="${this.open}">
-			</d2l-button-icon>
-			${this._renderHelpDialog()}
+			${this._renderDescriptionText()}
+		`;
+	}
+
+	_renderDescriptionText() {
+		return html`
+			<div>
+				<label>${this.localize('submissionViewHeading1')}</label>
+				<d2l-button-icon
+					icon="tier1:help"
+					@click="${this.open}">
+				</d2l-button-icon>
+				${this._renderHelpDialog()}
+			</div>
 		`;
 	}
 
 	_renderHelpDialog() {
-		const showSpinnerWhenLoading = true;
 		const width = 900;
 		return html`
 			<d2l-dialog
-				?async="${showSpinnerWhenLoading}"
 				?opened="${this.opened}"
 				width="${width}"
 				@d2l-dialog-close="${this.handleClose}"
