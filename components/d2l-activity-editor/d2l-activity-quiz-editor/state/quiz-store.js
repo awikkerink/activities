3a0a8430c93ac9eb2aeae9e83364e5b94a63c0ca
Vincent Wong <vincent.wong@d2l.com>
2021-10-05 12:47:58 -0700

    lint

--- components/d2l-activity-editor/d2l-activity-quiz-editor/state/quiz-store.js
+++ components/d2l-activity-editor/d2l-activity-quiz-editor/state/quiz-store.js
@@ -2,23 +2,12 @@ import { ObjectStore } from '../../state/object-store.js';
 import { Quiz } from './quiz.js';
 import { QuizAttempts } from './quiz-attempts.js';
 import { QuizIpRestrictions } from './quiz-ipRestrictions.js';
-import { QuizTiming } from './quiz-timing.js';
 import { QuizSubmissionViews } from './quiz-submission-views.js';
+import { QuizTiming } from './quiz-timing.js';
 
 export class QuizStore extends ObjectStore {
 	constructor() {
 		super(Quiz);
-	}
-}
-
-export class QuizIpRestrictionsStore extends ObjectStore {
-	constructor() {
-		super(QuizIpRestrictions);
-	}
-}
-export class QuizTimingStore extends ObjectStore {
-	constructor() {
-		super(QuizTiming);
 	}
 }
 
@@ -28,14 +17,26 @@ export class QuizAttemptsStore extends ObjectStore {
 	}
 }
 
+export class QuizIpRestrictionsStore extends ObjectStore {
+	constructor() {
+		super(QuizIpRestrictions);
+	}
+}
+
 export class QuizSubmissionViewsStore extends ObjectStore {
 	constructor() {
 		super(QuizSubmissionViews);
 	}
 }
 
+export class QuizTimingStore extends ObjectStore {
+	constructor() {
+		super(QuizTiming);
+	}
+}
+
 export const shared = new QuizStore();
-export const sharedIpRestrictions = new QuizIpRestrictionsStore();
-export const sharedTiming = new QuizTimingStore();
 export const sharedAttempts = new QuizAttemptsStore();
+export const sharedIpRestrictions = new QuizIpRestrictionsStore();
 export const sharedSubmissionViews = new QuizSubmissionViewsStore();
+export const sharedTiming = new QuizTimingStore();
