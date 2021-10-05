cdc02498bef89abb6014bf1be41a4a0f9a3468b8
Vincent Wong <vincent.wong@d2l.com>
2021-10-05 12:37:28 -0700

    Fix whitespace

--- components/d2l-activity-editor/d2l-activity-quiz-editor/state/quiz-submission-views.js
+++ components/d2l-activity-editor/d2l-activity-quiz-editor/state/quiz-submission-views.js
@@ -5,49 +5,49 @@ import { QuizSubmissionViewsEntity } from 'siren-sdk/src/activities/quizzes/subm
 configureMobx({ enforceActions: 'observed' });
 
 export class QuizSubmissionViews {
-	constructor(href, token) {
-		this.href = href;
-		this.token = token;
-		this.saving = null;
-	}
+    constructor(href, token) {
+        this.href = href;
+        this.token = token;
+        this.saving = null;
+    }
 
-	async fetch(bypassCache) {
-		const sirenEntity = await fetchEntity(this.href, this.token, bypassCache);
+    async fetch(bypassCache) {
+        const sirenEntity = await fetchEntity(this.href, this.token, bypassCache);
 
-		if (sirenEntity) {
-			const entity = new QuizSubmissionViewsEntity(sirenEntity, this.token, {
-				remove: () => { },
-			});
-			this.load(entity);
-		}
-		return this;
-	}
+        if (sirenEntity) {
+            const entity = new QuizSubmissionViewsEntity(sirenEntity, this.token, {
+                remove: () => { },
+            });
+            this.load(entity);
+        }
+        return this;
+    }
 
-	load(entity) {
+    load(entity) {
         this._entity = entity;
         this.canAddView = entity.canAddView();
-	}
+    }
 
-	async updateProperty(updateFunc) {
-		this.saving = updateFunc();
-		const entity = await this.saving;
-		this.saving = null;
-		// The siren-sdk function called to perform an action first checks that the entity has permission to do so.
-		// If the entity lacks permission, the function returns `undefined`, otherwise it returns a reconstructed siren-sdk submission views entity.
-		// If `undefined` is returned, it likely means the UI is out of sync with the entity state, and disallowed actions can be performed.
-		// In this case, we should attempt to reload the MobX object, so that the UI state is in sync again.
-		if (!entity) {
-			this.fetch();
-			return;
-		}
-		this._entity = entity;
-	}
+    async updateProperty(updateFunc) {
+        this.saving = updateFunc();
+        const entity = await this.saving;
+        this.saving = null;
+        // The siren-sdk function called to perform an action first checks that the entity has permission to do so.
+        // If the entity lacks permission, the function returns `undefined`, otherwise it returns a reconstructed siren-sdk submission views entity.
+        // If `undefined` is returned, it likely means the UI is out of sync with the entity state, and disallowed actions can be performed.
+        // In this case, we should attempt to reload the MobX object, so that the UI state is in sync again.
+        if (!entity) {
+            this.fetch();
+            return;
+        }
+        this._entity = entity;
+    }
 }
 
 decorate(QuizSubmissionViews, {
     // props
     canAddView: observable,
-	saving: observable,
-	// actions
-	load: action
+    saving: observable,
+    // actions
+    load: action
 });
