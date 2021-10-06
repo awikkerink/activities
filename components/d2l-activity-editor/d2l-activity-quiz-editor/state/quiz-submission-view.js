07c05037a9f59952291851c15237d126c9bbc42e
Vincent Wong <vincent.wong@d2l.com>
2021-10-06 16:09:33 -0700

    US132394 - Update MobX to get values from primary submission view siren response

--- components/d2l-activity-editor/d2l-activity-quiz-editor/state/quiz-submission-view.js
+++ components/d2l-activity-editor/d2l-activity-quiz-editor/state/quiz-submission-view.js
@@ -24,7 +24,29 @@ export class QuizSubmissionView {
 
 	load(entity) {
 		this._entity = entity;
+		this.canUpdateShowStandards = entity.canUpdateShowStandards();
+		this.canUpdateShowAttemptScore = entity.canUpdateShowAttemptScore();
+		this.canUpdateShowStatsClassAverage = entity.canUpdateShowStatsClassAverage();
+		this.canUpdateShowStatsScoreDistribution = entity.canUpdateShowStatsScoreDistribution();
 		this.isPrimaryView = entity.isPrimaryView();
+		this.showStandards = entity.showStandards();
+		this.showAttemptScore = entity.showAttemptScore();
+		this.showStatsClassAverage = entity.showStatsClassAverage();
+		this.showStatsScoreDistribution = entity.showStatsScoreDistribution();
+		this.canUpdateMessage = entity.canUpdateMessage();
+		this.messageText = entity.messageText();
+		this.messageHtml = entity.messageHtml();
+		this.canUpdateHideShowQuestions = entity.canUpdateHideShowQuestions();
+		this.hideQuestions = entity.hideQuestions();
+		this.canUpdateShowCorrectAnswers = entity.canUpdateShowCorrectAnswers();
+		this.canUpdateShowLearnerResponses = entity.canUpdateShowLearnerResponses();
+		this.canUpdateShowQuestions = entity.canUpdateShowQuestions();
+		this.canUpdateShowQuestionScore = entity.canUpdateShowQuestionScore();
+		this.showCorrectAnswers = entity.showCorrectAnswers();
+		this.showLearnerResponses = entity.showLearnerResponses();
+		this.showQuestionScore = entity.showQuestionScore();
+		this.showQuestionsType = entity.showQuestionsType();
+		this.showQuestionsOptions = entity.showQuestionsOptions();
 	}
 
 	async updateProperty(updateFunc) {
@@ -45,7 +67,29 @@ export class QuizSubmissionView {
 
 decorate(QuizSubmissionView, {
 	// props
+	canUpdateShowStandards: observable,
+	canUpdateShowAttemptScore: observable,
+	canUpdateShowStatsClassAverage: observable,
+	canUpdateShowStatsScoreDistribution: observable,
 	isPrimaryView: observable,
+	showStandards: observable,
+	showAttemptScore: observable,
+	showStatsClassAverage: observable,
+	showStatsScoreDistribution: observable,
+	canUpdateMessage: observable,
+	messageText: observable,
+	messageHtml: observable,
+	canUpdateHideShowQuestions: observable,
+	hideQuestions: observable,
+	canUpdateShowCorrectAnswers: observable,
+	canUpdateShowLearnerResponses: observable,
+	canUpdateShowQuestions: observable,
+	canUpdateShowQuestionScore: observable,
+	showCorrectAnswers: observable,
+	showLearnerResponses: observable,
+	showQuestionScore: observable,
+	showQuestionsType: observable,
+	showQuestionsOptions: observable,
 	// actions
 	load: action
 });
